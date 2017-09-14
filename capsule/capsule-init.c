753baaf8e0c0cfe00cd6c380cb6630badaa0bf5a
Vivek Das Mohapatra <vivek@collabora.co.uk>
2017-09-14 20:10:10 +0100

    Cache the "real" implementation of dlsym at init time
    The dlsym wrapper needs to cache the implementation of dlsym that's
    in the GOT at the point when it (the wrapper) is installed.
    
    Moving the implementation of the wrapper helper to capsule_init's
    file so that capsule_init can set some static state to do this for
    us.
    
    This is enough to make openarena work.

--- capsule/capsule-init.c
+++ capsule/capsule-init.c
@@ -1,10 +1,51 @@
 #include "capsule.h"
 #include "utils/utils.h"
 #include <stdlib.h>
+#include <dlfcn.h>
+
+typedef void * (*dlsymfunc) (void *handle, const char *symbol);
+static dlsymfunc symbol_lookup = NULL;
+
+static int
+dso_is_exported (const char *dsopath, const char **exported)
+{
+    for( const char **ex = exported; ex && *ex; ex++ )
+        if( soname_matches_path( *ex, dsopath ) )
+            return 1;
+
+    return 0;
+}
+
+void *
+capsule_shim_dlsym (void *capsule,
+                    void *handle,
+                    const char *symbol,
+                    const char **exported)
+{
+    void *addr = NULL;
+
+    if( (addr = symbol_lookup( capsule, symbol )) )
+    {
+        Dl_info dso = { 0 };
+
+        // only keep addr from the capsule if it's from an exported DSO:
+        // or if we are unable to determine where it came from (what?)
+        if( dladdr( addr, &dso ) )
+            if( !dso_is_exported( dso.dli_fname, exported ) )
+                addr = NULL;
+    }
+
+    if( addr == NULL )
+        addr = symbol_lookup( handle, symbol );
+
+    return addr;
+}
+
 
 void
 capsule_init (void)
 {
+    symbol_lookup = dlsym( RTLD_DEFAULT, "dlsym" );
     set_debug_flags( secure_getenv("CAPSULE_DEBUG") );
 }
 

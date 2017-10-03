7d34220d4421c52c92a48d38864639b70add2978
Simon McVittie <smcv@collabora.com>
2017-10-03 16:27:06 +0100

    Provide _ext_dlsym(), etc. via headers capsule/_ext_dlsym.h etc.
    This lets library users override them via the usual compiler include
    path.
    
    Signed-off-by: Simon McVittie <smcv@collabora.com>

---
+++ capsule/_int_dlopen.h
@@ -0,0 +1,33 @@
+// Copyright © 2017 Collabora Ltd
+//
+// This file is part of libcapsule.
+//
+// libcapsule is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 2.1 of the
+// License, or (at your option) any later version.
+//
+// libcapsule is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with libcapsule.  If not, see <http://www.gnu.org/licenses/>.
+
+// Standard implementation of _int_dlopen(), the implementation used
+// when a library inside the capsule calls dlopen(). To override this
+// implementation, copy this file to shim/capsule/_int_dlopen.h in
+// your shim library and modify as needed.
+static void *
+_int_dlopen (const char *filename, int flag)
+{
+    if( flag & RTLD_GLOBAL )
+    {
+        fprintf( stderr, "Warning: libcapsule dlopen wrapper cannot pass "
+                         "RTLD_GLOBAL to underlying dlmopen(%s...) call\n",
+                 filename );
+        flag = (flag & ~RTLD_GLOBAL) & 0xfffff;
+    }
+    return capsule_shim_dlopen( cap, filename, flag );
+}

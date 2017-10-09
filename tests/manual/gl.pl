697d46bccbf01ea90b4b069277fbb08df3b816ba
Simon McVittie <smcv@collabora.com>
2017-10-09 14:37:48 +0100

    tests/manual/gl.pl: Treat more X libraries as core libraries
    We need to sync up libX11, libX11-xcb, libcapsule and libelf anyway,
    so while we're experimenting with what works and what crashes,
    we might as well exclude more of the X11/xcb family from encapsulation.
    
    We might be able to take some or all of these out again when we have
    everything working.
    
    Signed-off-by: Simon McVittie <smcv@collabora.com>

--- tests/manual/gl.pl
+++ tests/manual/gl.pl
@@ -938,7 +938,9 @@ do {
     capture_gl_provider_libs_if_newer($gl_provider_tree, $container_tree,
         \@multiarch_tuples, $gl_provider_updates,
         [qw(X11 X11-xcb capsule elf xcb xcb-dri2 xcb-dri3 xcb-glx
-            xcb-present xcb-sync)]);
+            xcb-present xcb-sync xcb-xfixes
+            Xau Xdamage Xdmcp Xext Xfixes Xxf86vm
+            )]);
 
     foreach my $tuple (@multiarch_tuples) {
         push @ld_path, "/updates/lib/$tuple";

0d5bd6c8e0e83b97eee4220aec65678c0c479c48
Simon McVittie <smcv@collabora.com>
2019-11-12 13:40:22 +0000

    Revert "_GLNX_TEST_SCOPED_TEMP_DIR: Fix memory and fd leak"
    This accidentally requires GLib 2.36.
    
    This reverts commit a652ede20b6bc4726fbbce41ea4657fc83d8c6bb.
    
    Signed-off-by: Simon McVittie <smcv@collabora.com>

--- subprojects/libglnx/tests/libglnx-testlib.c
+++ subprojects/libglnx/tests/libglnx-testlib.c
@@ -23,8 +23,6 @@
 
 #include <errno.h>
 
-#include <glib/gstdio.h>
-
 #include "libglnx.h"
 
 struct _GLnxTestAutoTempDir
@@ -65,10 +63,4 @@ _glnx_test_auto_temp_dir_leave (_GLnxTestAutoTempDir *dir)
 
   glnx_tmpdir_delete (&dir->temp_dir, NULL, &error);
   g_assert_no_error (error);
-
-  g_close (dir->old_cwd_fd, &error);
-  g_assert_no_error (error);
-
-  g_free (dir->old_cwd);
-  g_free (dir);
 }

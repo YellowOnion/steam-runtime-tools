YellowOnion/steam-runtime-tools

commit 959fd33807d811787d04123ecd9afbc83e6b1412

wrap: Take out a lock on the container's runtime for the duration
If we upgrade from runtime 0.20191024.0 to 0.20191108.0 while an app
or game is still running under runtime 0.20191024.0, then deleting
the app or game's shared libraries out from under it will probably
break it.

Flatpak solves this problem with a read/write lock on the file
`files/.ref` - for runtimes this is `/usr/.ref` in the container.
Each instance of `flatpak run` takes a read-lock on that file. Any
number of processes can hold a read-lock at the same time.
Before a package-manager-style operation like `flatpak uninstall`
does anything destructive to a runtime, it takes a write-lock, which
only succeeds if there are currently no read- or write-locks.
Depending on the operation, the lock is either taken directly or by
asking bubblewrap to do it.

We'll use the same scheme. Different families of locking APIs interact
unpredictably, so it's important that we use the same locking API as
bubblewrap, namely POSIX byte-range (record) advisory locks via
fcntl F_SETLK. In particular, this means flock(1) and flock(2) are
not going to help us: they use POSIX file-level advisory locks.

One difference between our locking and flatpak's is that we support
runtimes that are not "merged /usr". In this case, we could either
take a lock on files/.ref (/.ref in the container) or on files/usr/.ref
(/usr/.ref in the container). I've chosen to do the former here, so that
it's always files/.ref. It doesn't matter, as long as we're consistent.

(For apps the lock file is `/app/.ref` and for extensions it's
`$mountpoint/.ref`, but pressure-vessel doesn't have an equivalent of
Flatpak apps or extensions yet.)

This version is only a partial solution. We can't use bwrap --lock-file
or bwrap --sync-fd without also using --unshare-pid, which breaks
gameoverlayrenderer.so; so we do the next best thing, by leaking the
lock fd to the bubblewrap process, which will leak it to the game's

src/wrap.c
@@ -38,6 +38,7 @@
 #include <steam-runtime-tools/steam-runtime-tools.h>
 
 #include "bwrap.h"
+#include "bwrap-lock.h"
 #include "flatpak-bwrap-private.h"
 #include "flatpak-run-private.h"
 #include "flatpak-utils-private.h"
@@ -1968,6 +1969,7 @@ main (int argc,
   g_autoptr(GString) adjusted_ld_preload = g_string_new ("");
   g_autofree gchar *cwd_p = NULL;
   g_autofree gchar *cwd_l = NULL;
+  g_autoptr(PvBwrapLock) runtime_lock = NULL;
   const gchar *home;
   g_autofree gchar *bwrap_help = NULL;
   g_autofree gchar *tools_dir = NULL;
@@ -2329,7 +2331,36 @@ main (int argc,
 
   if (opt_runtime != NULL && opt_runtime[0] != '\0')
     {
+      g_autofree gchar *usr = NULL;
+      g_autofree gchar *files_ref = NULL;
+
       g_debug ("Configuring runtime...");
+
+      /* Take a lock on the runtime until we're finished with setup,
+       * to make sure it doesn't get deleted. */
+      files_ref = g_build_filename (opt_runtime, ".ref", NULL);
+      runtime_lock = pv_bwrap_lock_new (files_ref,
+                                        PV_BWRAP_LOCK_FLAGS_CREATE,
+                                        error);
+
+      /* If the runtime is being deleted, ... don't use it, I suppose? */
+      if (runtime_lock == NULL)
+        goto out;
+
+      usr = g_build_filename (opt_runtime, "usr", NULL);
+
+      /* Tell bwrap to hold a reference to files/.ref until it exits.
+       * If files is a merged-/usr, it's mounted as /usr (as if for
+       * Flatpak); otherwise it's mounted at /, which we need to cope
+       * with too. */
+      if (g_file_test (usr, G_FILE_TEST_IS_DIR))
+        flatpak_bwrap_add_args (bwrap,
+                                "--lock-file", "/.ref",
+                                NULL);
+      else
+        flatpak_bwrap_add_args (bwrap,
+                                "--lock-file", "/usr/.ref",
+                                NULL);
 
       search_path_append (bin_path, "/overrides/bin");
       search_path_append (bin_path, g_getenv ("PATH"));
@@ -2571,6 +2602,18 @@ main (int argc,
     }
 
   g_clear_pointer (&tmpdir, g_free);
+
+  /* If we are using a runtime, pass the lock fd to the bwrap process.
+   * We hope it will hold the fd open for as long as it's running.
+   *
+   * This is not a complete solution - what we really want is for the
+   * bwrap process *and every one of its children, recursively* to hold
+   * this fd open. However, we can't currently use bwrap --lock-file
+   * or bwrap --sync-fd without --unshare-pid
+   * (<https://github.com/containers/bubblewrap/issues/336>), and we
+   * can't do that without breaking gameoverlayrender.so's assumptions. */
+  if (runtime_lock != NULL)
+    flatpak_bwrap_add_fd (bwrap, pv_bwrap_lock_steal_fd (runtime_lock));
 
   flatpak_bwrap_finish (bwrap);
   pv_bwrap_execve (bwrap, error);

commit d47b241c726dff8f06319b9643a3574b0d299d09

wrap: Tell child process to take out its own lock if necessary
Non-OFD locks don't propagate across fork(), and bwrap needs to clone()
itself (which behaves like fork() in this respect) to separate itself
into a parent outside the container and a child inside the container.

This change adds a weak dependency on Linux 3.15. If we run on an older
version, everything should still *work*, but there will be a short
period of time during which we have already decided to use the runtime,
but it is not locked (and in particular not protected from deletion).

Fixes: 959fd338 "wrap: Take out a lock on the container's runtime for the duration"
Signed-off-by: Simon McVittie <smcv@collabora.com>

src/bwrap.c
@@ -226,7 +226,8 @@ pv_bwrap_bind_usr (FlatpakBwrap *bwrap,
     {
       if (g_str_has_prefix (member, "lib")
           || g_str_equal (member, "bin")
-          || g_str_equal (member, "sbin"))
+          || g_str_equal (member, "sbin")
+          || g_str_equal (member, ".ref"))
         {
           dest = g_build_filename (mount_point, member, NULL);
 

src/wrap.c
@@ -4,7 +4,7 @@
  * Contains code taken from Flatpak.
  *
  * Copyright © 2014-2019 Red Hat, Inc
- * Copyright © 2017-2019 Collabora Ltd.
+ * Copyright © 2017-2020 Collabora Ltd.
  *
  * SPDX-License-Identifier: LGPL-2.1-or-later
  *
@@ -2568,15 +2568,46 @@ main (int argc,
    * can't do that without breaking gameoverlayrender.so's assumptions. */
   if (runtime_lock != NULL)
     {
-      g_autofree gchar *fd_str = NULL;
-      int fd = pv_bwrap_lock_steal_fd (runtime_lock);
-
-      fd_str = g_strdup_printf ("%d", fd);
-      flatpak_bwrap_add_fd (bwrap, fd);
       flatpak_bwrap_add_args (bwrap,
                               "/run/pressure-vessel/bin/pressure-vessel-with-lock",
                               "--subreaper",
-                              "--fd", fd_str,
+                              NULL);
+
+      if (pv_bwrap_lock_is_ofd (runtime_lock))
+        {
+          int fd = pv_bwrap_lock_steal_fd (runtime_lock);
+          g_autofree gchar *fd_str = NULL;
+
+          g_debug ("Passing lock fd %d down to with-lock", fd);
+          flatpak_bwrap_add_fd (bwrap, fd);
+          fd_str = g_strdup_printf ("%d", fd);
+          flatpak_bwrap_add_args (bwrap,
+                                  "--fd", fd_str,
+                                  NULL);
+        }
+      else
+        {
+          /*
+           * We were unable to take out an open file descriptor lock,
+           * so it will be released on fork(). Tell the with-lock process
+           * to take out its own compatible lock instead. There will be
+           * a short window during which we have lost our lock but the
+           * with-lock process has not taken its lock - that's unavoidable
+           * if we want to use exec() to replace ourselves with the
+           * container.
+           *
+           * pv_bwrap_bind_usr() arranges for /.ref to either be a
+           * symbolic link to /usr/.ref which is the runtime_lock
+           * (if opt_runtime is a merged /usr), or the runtime_lock
+           * itself (otherwise).
+           */
+          g_debug ("Telling process in container to lock /.ref");
+          flatpak_bwrap_add_args (bwrap,
+                                  "--lock-file", "/.ref",
+                                  NULL);
+        }
+
+      flatpak_bwrap_add_args (bwrap,
                               "--",
                               NULL);
     }
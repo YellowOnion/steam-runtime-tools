YellowOnion/steam-runtime-tools

commit ed218cedc15813b5d0c512936cd2848945067b3a

Add --version argument to most executables
This makes it easier to tell which version we are dealing with.

Signed-off-by: Simon McVittie <smcv@collabora.com>

bin/system-info.c
@@ -129,6 +129,7 @@ enum
   OPTION_HELP = 1,
   OPTION_EXPECTATION,
   OPTION_VERBOSE,
+  OPTION_VERSION,
 };
 
 struct option long_options[] =
@@ -482,6 +483,16 @@ main (int argc,
           case OPTION_VERBOSE:
             verbose = TRUE;
             break;
+
+          case OPTION_VERSION:
+            /* Output version number as YAML for machine-readability,
+             * inspired by `ostree --version` and `docker version` */
+            printf (
+                "%s:\n"
+                " Package: steam-runtime-tools\n"
+                " Version: %s\n",
+                argv[0], VERSION);
+            return 0;
 
           case OPTION_HELP:
             usage (0);

helpers/check-locale.c
@@ -151,6 +151,7 @@ mock_get_charset (const char **charset)
 #endif
 
 static gchar *opt_locale = NULL;
+static gboolean opt_print_version = FALSE;
 
 static gboolean
 opt_locale_cb (const char *name,
@@ -171,6 +172,8 @@ opt_locale_cb (const char *name,
 
 static const GOptionEntry option_entries[] =
 {
+  { "version", 0, G_OPTION_FLAG_NONE, G_OPTION_ARG_NONE, &opt_print_version,
+    "Print version number and exit", NULL },
   { G_OPTION_REMAINING, 0, G_OPTION_FLAG_NONE, G_OPTION_ARG_CALLBACK,
     opt_locale_cb,
     "The locale to test [default: use environment variables]",
@@ -200,6 +203,19 @@ main (int argc,
   if (!g_option_context_parse (option_context, &argc, &argv, &local_error))
     {
       ret = 2;
+      goto out;
+    }
+
+  if (opt_print_version)
+    {
+      /* Output version number as YAML for machine-readability,
+       * inspired by `ostree --version` and `docker version` */
+      g_print (
+          "%s:\n"
+          " Package: steam-runtime-tools\n"
+          " Version: %s\n",
+          argv[0], VERSION);
+      ret = 0;
       goto out;
     }
 

helpers/check-vulkan.cpp
@@ -925,10 +925,12 @@ private:
 
 enum {
     OPTION_HELP = 1,
+    OPTION_VERSION,
 };
 
 static struct option long_options[] = {
     { "help", no_argument, NULL, OPTION_HELP },
+    { "version", no_argument, NULL, OPTION_VERSION },
     { NULL, 0, NULL, 0 }
 };
 
@@ -939,6 +941,7 @@ static void usage(int code) {
     stream << "Usage: " << argv0 << " [OPTIONS]" << std::endl;
     stream << "Options:" << std::endl;
     stream << "--help\t\tShow this help and exit" << std::endl;
+    stream << "--version\tShow version and exit" << std::endl;
     std::exit(code);
 }
 
@@ -953,6 +956,14 @@ int main(int argc, char** argv) {
             case OPTION_HELP:
                 usage(0);
                 break;  // not reached
+
+            case OPTION_VERSION:
+                /* Output version number as YAML for machine-readability,
+                 * inspired by `ostree --version` and `docker version` */
+                std::cout << argv[0] << ":" << std::endl
+                    << " Package: steam-runtime-tools" << std::endl
+                    << " Version: " << VERSION << std::endl;
+                return EXIT_SUCCESS;
 
             case '?':
             default:

helpers/inspect-library.c
@@ -77,6 +77,7 @@ enum
   OPTION_HELP = 1,
   OPTION_DEB_SYMBOLS,
   OPTION_HIDDEN_DEPENDENCY,
+  OPTION_VERSION,
 };
 
 struct option long_options[] =
@@ -84,6 +85,7 @@ struct option long_options[] =
     { "hidden-dependency", required_argument, NULL, OPTION_HIDDEN_DEPENDENCY },
     { "deb-symbols", no_argument, NULL, OPTION_DEB_SYMBOLS },
     { "help", no_argument, NULL, OPTION_HELP },
+    { "version", no_argument, NULL, OPTION_VERSION },
     { NULL, 0, NULL, 0 }
 };
 
@@ -146,6 +148,16 @@ main (int argc,
           case OPTION_HELP:
             usage (0);
             break;
+
+          case OPTION_VERSION:
+            /* Output version number as YAML for machine-readability,
+             * inspired by `ostree --version` and `docker version` */
+            printf (
+                "%s:\n"
+                " Package: steam-runtime-tools\n"
+                " Version: %s\n",
+                argv[0], VERSION);
+            return 0;
 
           case '?':
           default:

meson.build
@@ -106,9 +106,18 @@ foreach flag : no_warning_cflags
   add_project_arguments(supported_no_warning_cflags, language : 'c')
 endforeach
 
-add_project_arguments(['-std=c99'], language : 'c')
+conf_data = configuration_data()
+conf_data.set_quoted('VERSION', meson.project_version())
 
+configure_file(
+  output : 'config.h',
+  configuration : conf_data,
+)
+
+add_project_arguments(['-std=c99'], language : 'c')
 add_project_arguments(['-D_GNU_SOURCE'], language : 'c')
+add_project_arguments(['-include', 'config.h'], language : 'c')
+add_project_arguments(['-include', 'config.h'], language : 'cpp')
 
 glib = dependency(
   'glib-2.0',

commit f8f3f6e7ec42a5e5ba0559496c5e23618fb088b7

s-r-s-i: Really add --version option
This was half-implemented in commit ed218ce.

Fixes: ed218ced "Add --version argument to most executables"
Signed-off-by: Simon McVittie <smcv@collabora.com>

bin/system-info.c
@@ -139,6 +139,7 @@ struct option long_options[] =
 {
     { "expectations", required_argument, NULL, OPTION_EXPECTATION },
     { "verbose", no_argument, NULL, OPTION_VERBOSE },
+    { "version", no_argument, NULL, OPTION_VERSION },
     { "help", no_argument, NULL, OPTION_HELP },
     { NULL, 0, NULL, 0 }
 };
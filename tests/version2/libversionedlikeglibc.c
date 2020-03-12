31ec064979447cda6d7971b766d8edc863e45a15
Simon McVittie <smcv@collabora.com>
2020-03-12 16:38:22 +0000

    tests: Make older linkers happy with one of our test libraries
    The linker used in Steam Runtime 1 'scout', which is from several years
    ago, doesn't seem to like a symbol being made an alias for a versioned
    symbol of the same name. Rename the symbol so that only the versioned
    aliases get the same basename.
    
    Signed-off-by: Simon McVittie <smcv@collabora.com>

--- tests/version2/libversionedlikeglibc.c
+++ tests/version2/libversionedlikeglibc.c
@@ -3,6 +3,6 @@ int symbol1(int x) { return x; }
 __asm__(".symver _original_symbol2,symbol2@LIBVERSIONED1");
 __attribute__((__visibility__("default"))) int _original_symbol2(int);
 int _original_symbol2(int x) { return x; }
-__asm__(".symver symbol2,symbol2@@LIBVERSIONED2");
-__attribute__((__visibility__("default"))) int symbol2(int);
-int symbol2(int x) { return x + 1; }
+__asm__(".symver _new_symbol2,symbol2@@LIBVERSIONED2");
+__attribute__((__visibility__("default"))) int _new_symbol2(int);
+int _new_symbol2(int x) { return x + 1; }

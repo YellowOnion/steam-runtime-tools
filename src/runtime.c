YellowOnion/steam-runtime-tools

commit 5e2f9f259cc22743b90d58f51fd4425673e47818

runtime: Bind-mount /overrides to run locale-gen
Even in the case where we're using a temporary directory, it's going
to survive at least as long as the call to locale-gen, so we don't
need to translate it into a long series of --dir and --symlink
directives.

Signed-off-by: Simon McVittie <smcv@collabora.com>

src/runtime.c
@@ -1120,7 +1120,9 @@ ensure_locales (PvRuntime *self,
                                      NULL);
 
       flatpak_bwrap_append_bwrap (run_locale_gen, bwrap);
-      pv_bwrap_copy_tree (run_locale_gen, self->overrides, "/overrides");
+      flatpak_bwrap_add_args (bwrap,
+                              "--ro-bind", self->overrides, "/overrides",
+                              NULL);
 
       if (!flatpak_bwrap_bundle_args (run_locale_gen, 1, -1, FALSE,
                                       &local_error))

commit 8710f9f312949c25d9480e9aab280f1b77e9c436

runtime: Add arguments to the right bwrap invocation
When we're entering the container to generate locales, we need to
bind-mount the staging area for /overrides in *that* bwrap invocation,
not add it as a bind-mount to the bwrap invocation that will actually
run the game.

Fixes: 5e2f9f25 "runtime: Bind-mount /overrides to run locale-gen"
Signed-off-by: Simon McVittie <smcv@collabora.com>

src/runtime.c
@@ -1169,7 +1169,7 @@ ensure_locales (PvRuntime *self,
                                      NULL);
 
       flatpak_bwrap_append_bwrap (run_locale_gen, bwrap);
-      flatpak_bwrap_add_args (bwrap,
+      flatpak_bwrap_add_args (run_locale_gen,
                               "--ro-bind", self->overrides,
                               self->overrides_in_container,
                               NULL);
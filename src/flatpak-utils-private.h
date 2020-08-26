936573fa0784e1638d41968189a1e41cbab6daca
Simon McVittie <smcv@collabora.com>
2020-08-26 14:47:10 +0100

    Add flatpak-context, flatpak-exports from Flatpak
    A lot of the code added here is #if 0. Normally we don't commit
    commented-out or #ifdef'd out code, but in this case it helps tools
    like gvimdiff to have enough context to figure out which parts of the
    file in Flatpak correspond to which parts of the file in
    pressure-vessel.
    
    Signed-off-by: Simon McVittie <smcv@collabora.com>

--- src/flatpak-utils-private.h
+++ src/flatpak-utils-private.h
@@ -24,6 +24,22 @@
 #define __FLATPAK_UTILS_H__
 
 #include "libglnx/libglnx.h"
+#include <flatpak-common-types-private.h>
+#include <gio/gio.h>
+#include <gio/gunixfdlist.h>
+#include "flatpak-context-private.h"
+#include "flatpak-error.h"
+
+#define AUTOFS_SUPER_MAGIC 0x0187
+
+/* https://bugzilla.gnome.org/show_bug.cgi?id=766370 */
+#if !GLIB_CHECK_VERSION (2, 49, 3)
+#define FLATPAK_VARIANT_BUILDER_INITIALIZER {{0, }}
+#define FLATPAK_VARIANT_DICT_INITIALIZER {{0, }}
+#else
+#define FLATPAK_VARIANT_BUILDER_INITIALIZER {{{0, }}}
+#define FLATPAK_VARIANT_DICT_INITIALIZER {{{0, }}}
+#endif
 
 /* https://github.com/GNOME/libglnx/pull/38
  * Note by using #define rather than wrapping via a static inline, we
@@ -31,20 +47,49 @@
  */
 #define flatpak_fail glnx_throw
 
+gboolean flatpak_fail_error (GError     **error,
+                             FlatpakError code,
+                             const char  *fmt,
+                             ...) G_GNUC_PRINTF (3, 4);
+
 #define flatpak_debug2 g_debug
+
+gint flatpak_strcmp0_ptr (gconstpointer a,
+                          gconstpointer b);
 
 /* Sometimes this is /var/run which is a symlink, causing weird issues when we pass
  * it as a path into the sandbox */
 char * flatpak_get_real_xdg_runtime_dir (void);
 
+gboolean  flatpak_has_path_prefix (const char *str,
+                                   const char *prefix);
+
+
+gboolean flatpak_g_ptr_array_contains_string (GPtrArray  *array,
+                                              const char *str);
 
 char * flatpak_quote_argv (const char *argv[],
                            gssize      len);
 
+const char *flatpak_file_get_path_cached (GFile *file);
+
+
+gboolean flatpak_mkdir_p (GFile        *dir,
+                          GCancellable *cancellable,
+                          GError      **error);
 gboolean flatpak_buffer_to_sealed_memfd_or_tmpfile (GLnxTmpfile *tmpf,
                                                     const char  *name,
                                                     const char  *str,
                                                     size_t       len,
                                                     GError     **error);
 
+#if !GLIB_CHECK_VERSION (2, 43, 4)
+G_DEFINE_AUTOPTR_CLEANUP_FUNC (GUnixFDList, g_object_unref)
+#endif
+
+static inline void
+null_safe_g_ptr_array_unref (gpointer data)
+{
+  g_clear_pointer (&data, g_ptr_array_unref);
+}
 #endif /* __FLATPAK_UTILS_H__ */

YellowOnion/steam-runtime-tools

commit 3a8e939d941936650cb388e3083751b02c5f9163

pv-wrap: Delay --dir, etc. until after FlatpakExports are processed
If the FlatpakExports has set these up to be symlinks, it's harmless
to use --dir redundantly; but if we do these too soon, the
FlatpakExports would be unable to create its symlinks and would fail.

Resolves: https://github.com/ValveSoftware/steam-runtime/issues/368
Signed-off-by: Simon McVittie <smcv@collabora.com>

pressure-vessel/wrap.c
@@ -1278,6 +1278,7 @@ main (int argc,
   gboolean is_flatpak_env = g_file_test ("/.flatpak-info", G_FILE_TEST_IS_REGULAR);
   g_autoptr(PvEnviron) container_env = NULL;
   g_autoptr(FlatpakBwrap) bwrap = NULL;
+  g_autoptr(FlatpakBwrap) bwrap_filesystem_arguments = NULL;
   g_autoptr(FlatpakBwrap) argv_in_container = NULL;
   g_autoptr(FlatpakBwrap) final_argv = NULL;
   g_autoptr(FlatpakExports) exports = NULL;
@@ -1736,6 +1737,7 @@ main (int argc,
       /* Start with an empty environment and populate it later */
       bwrap = flatpak_bwrap_new (flatpak_bwrap_empty_env);
       flatpak_bwrap_add_arg (bwrap, bwrap_executable);
+      bwrap_filesystem_arguments = flatpak_bwrap_new (flatpak_bwrap_empty_env);
       exports = flatpak_exports_new ();
     }
 
@@ -1765,7 +1767,7 @@ main (int argc,
 
       /* Start with just the root tmpfs (which appears automatically)
        * and the standard API filesystems */
-      pv_bwrap_add_api_filesystems (bwrap);
+      pv_bwrap_add_api_filesystems (bwrap_filesystem_arguments);
 
       /* The FlatpakExports will populate /run/host for us */
       flatpak_exports_add_host_etc_expose (exports,
@@ -1775,7 +1777,7 @@ main (int argc,
 
       /* steam-runtime-system-info uses this to detect pressure-vessel, so we
        * need to create it even if it will be empty */
-      flatpak_bwrap_add_args (bwrap,
+      flatpak_bwrap_add_args (bwrap_filesystem_arguments,
                               "--dir",
                               "/run/pressure-vessel",
                               NULL);
@@ -1867,7 +1869,7 @@ main (int argc,
 
       if (!pv_runtime_bind (runtime,
                             exports,
-                            bwrap,
+                            bwrap_filesystem_arguments,
                             container_env,
                             error))
         goto out;
@@ -1883,16 +1885,17 @@ main (int argc,
     {
       g_assert (!is_flatpak_env);
 
-      if (!pv_wrap_use_host_os (exports, bwrap, error))
+      if (!pv_wrap_use_host_os (exports, bwrap_filesystem_arguments, error))
         goto out;
     }
 
   /* Protect other users' homes (but guard against the unlikely
-   * situation that they don't exist) */
+   * situation that they don't exist). We use the FlatpakExports for this
+   * so that it can be overridden by --filesystem=/home, and so that it
+   * is sorted correctly with respect to all the other
+   * home-directory-related exports. */
   if (g_file_test ("/home", G_FILE_TEST_EXISTS))
-    flatpak_bwrap_add_args (bwrap,
-                            "--tmpfs", "/home",
-                            NULL);
+    flatpak_exports_add_path_tmpfs (exports, "/home");
 
   g_debug ("Making home directory available...");
 
@@ -1920,7 +1923,8 @@ main (int argc,
         }
       else
         {
-          if (!use_fake_home (exports, bwrap, container_env, opt_fake_home,
+          if (!use_fake_home (exports, bwrap_filesystem_arguments,
+                              container_env, opt_fake_home,
                               error))
             goto out;
         }
@@ -2139,6 +2143,13 @@ main (int argc,
       adjust_exports (exports_bwrap, home);
       g_warn_if_fail (g_strv_length (exports_bwrap->envp) == 0);
       flatpak_bwrap_append_bwrap (bwrap, exports_bwrap);
+
+      /* The other filesystem arguments have to come after the exports
+       * so that if the exports set up symlinks, the other filesystem
+       * arguments like --dir work with the symlinks' targets. */
+      g_warn_if_fail (g_strv_length (bwrap_filesystem_arguments->envp) == 0);
+      flatpak_bwrap_append_bwrap (bwrap, bwrap_filesystem_arguments);
+      g_clear_pointer (&bwrap_filesystem_arguments, flatpak_bwrap_free);
     }
 
   if (bwrap != NULL)

commit 242e8906a4ae2a6c36f08fe27c7dfb9e13af1666

wrap: Put game-specific $HOME on bwrap command-line before exports
This is how Flatpak behaves, and it's necessary to prevent the
game-specific $HOME from being mounted over the top of some of the
exports, preventing (for example) ~/.steam from being exposed to the
container.

We only do this for $HOME rather than completely reverting 3a8e939d,
so that it won't be a regression for a symlink at /var/tmp.

If the logical and physical paths to the real $HOME differ, we now also
mount the game-specific $HOME onto the real $HOME's physical path, and
replicate the logical path's symlinks in the container. This matches how
Flatpak works, and avoids a similar regression.

Fixes: 3a8e939d "pv-wrap: Delay --dir, etc. until after FlatpakExports are processed"
Resolves: https://gitlab.steamos.cloud/steamrt/steam-runtime-tools/-/issues/63
Signed-off-by: Simon McVittie <smcv@collabora.com>

pressure-vessel/wrap.c
@@ -475,8 +475,18 @@ use_fake_home (FlatpakExports *exports,
                                         data2);
     }
 
+  /* If the logical path to real_home has a symlink among its ancestors
+   * (e.g. /home/user when /home -> var/home exists), make sure the
+   * symlink structure gets mirrored in the container */
+  flatpak_exports_add_path_dir (exports, g_get_home_dir ());
+
+  /* Mount the fake home directory onto the physical path to real_home,
+   * so that it will not conflict with symlinks created by the exports.
+   * See also https://github.com/flatpak/flatpak/issues/1278 and
+   * Flatpak commit f1df5cb1 */
+  flatpak_bwrap_add_bind_arg (bwrap, "--bind", fake_home, real_home);
+
   flatpak_bwrap_add_args (bwrap,
-                          "--bind", fake_home, real_home,
                           "--bind", tmp, "/var/tmp",
                           NULL);
 
@@ -1296,6 +1306,7 @@ main (int argc,
   g_autoptr(PvEnviron) container_env = NULL;
   g_autoptr(FlatpakBwrap) bwrap = NULL;
   g_autoptr(FlatpakBwrap) bwrap_filesystem_arguments = NULL;
+  g_autoptr(FlatpakBwrap) bwrap_home_arguments = NULL;
   g_autoptr(FlatpakBwrap) argv_in_container = NULL;
   g_autoptr(FlatpakBwrap) final_argv = NULL;
   g_autoptr(FlatpakExports) exports = NULL;
@@ -2090,7 +2101,9 @@ main (int argc,
         }
       else
         {
-          if (!use_fake_home (exports, bwrap_filesystem_arguments,
+          bwrap_home_arguments = flatpak_bwrap_new (flatpak_bwrap_empty_env);
+
+          if (!use_fake_home (exports, bwrap_home_arguments,
                               container_env, opt_fake_home,
                               error))
             goto out;
@@ -2374,6 +2387,17 @@ main (int argc,
 
       g_assert (bwrap != NULL);
       g_assert (bwrap_filesystem_arguments != NULL);
+
+      if (bwrap_home_arguments != NULL)
+        {
+          /* The filesystem arguments to set up a fake $HOME (if any) have
+           * to come before the exports, as they do in Flatpak, so that
+           * mounting the fake $HOME will not mask the exports used for
+           * ~/.steam, etc. */
+          g_warn_if_fail (g_strv_length (bwrap_home_arguments->envp) == 0);
+          flatpak_bwrap_append_bwrap (bwrap, bwrap_home_arguments);
+          g_clear_pointer (&bwrap_home_arguments, flatpak_bwrap_free);
+        }
 
       flatpak_exports_append_bwrap_args (exports, exports_bwrap);
       adjust_exports (exports_bwrap, home);
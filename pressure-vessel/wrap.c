YellowOnion/steam-runtime-tools

commit 1b720eb78dc09ff22538f7005965bd5065278cab

wrap: Set PulseAudio as the default ALSA driver if available
To avoid audio issues, if we have successfully bound a PulseAudio
socket, we should also set it as the default driver in ALSA.

Addresses: #65
Partially addresses:
https://github.com/ValveSoftware/steam-runtime/issues/371

Signed-off-by: Ludovico de Nittis <ludovico.denittis@collabora.com>

pressure-vessel/wrap.c
@@ -2417,6 +2417,21 @@ main (int argc,
                            (runtime != NULL),
                            is_flatpak_env);
 
+  if (pv_environ_is_locked(container_env, "PULSE_SERVER")
+      || is_flatpak_env)
+    {
+      /* Make the PulseAudio driver the default.
+       * We do this unconditionally when we are under Flatpak for parity
+       * with the freedesktop.org Platform. */
+      const gchar *alsa_config = "pcm.!default pulse\n"
+                                 "ctl.!default pulse\n";
+
+      flatpak_bwrap_add_args_data (bwrap, "99-pulseaudio-default.conf",
+                                   alsa_config, -1,
+                                   "/etc/alsa/conf.d/99-pulseaudio-default.conf",
+                                   NULL);
+    }
+
   if (is_flatpak_env)
     {
       g_autoptr(GList) vars = NULL;

commit 69294cbd2c3507d91a7c1b73a78b9b0f43ab8c72

pv-wrap: Don't crash with Flatpak subsandbox
Fixes: 1b720eb7 "wrap: Set PulseAudio as the default ALSA driver if available"
Signed-off-by: Simon McVittie <smcv@collabora.com>

pressure-vessel/wrap.c
@@ -2419,7 +2419,8 @@ main (int argc,
 
   if (runtime != NULL
       && (pv_environ_is_locked(container_env, "PULSE_SERVER")
-          || is_flatpak_env))
+          || is_flatpak_env)
+      && bwrap != NULL)
     {
       /* Make the PulseAudio driver the default.
        * We do this unconditionally when we are under Flatpak for parity
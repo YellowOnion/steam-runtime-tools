YellowOnion/steam-runtime-tools

commit 1b720eb78dc09ff22538f7005965bd5065278cab

wrap: Set PulseAudio as the default ALSA driver if available
To avoid audio issues, if we have successfully bound a PulseAudio
socket, we should also set it as the default driver in ALSA.

Addresses: #65
Partially addresses:
https://github.com/ValveSoftware/steam-runtime/issues/371

Signed-off-by: Ludovico de Nittis <ludovico.denittis@collabora.com>

pressure-vessel/wrap.c
@@ -2417,6 +2417,21 @@ main (int argc,
                            (runtime != NULL),
                            is_flatpak_env);
 
+  if (pv_environ_is_locked(container_env, "PULSE_SERVER")
+      || is_flatpak_env)
+    {
+      /* Make the PulseAudio driver the default.
+       * We do this unconditionally when we are under Flatpak for parity
+       * with the freedesktop.org Platform. */
+      const gchar *alsa_config = "pcm.!default pulse\n"
+                                 "ctl.!default pulse\n";
+
+      flatpak_bwrap_add_args_data (bwrap, "99-pulseaudio-default.conf",
+                                   alsa_config, -1,
+                                   "/etc/alsa/conf.d/99-pulseaudio-default.conf",
+                                   NULL);
+    }
+
   if (is_flatpak_env)
     {
       g_autoptr(GList) vars = NULL;

commit c369b2cdc4a7839e5f1d920f702ea32ae7b29fed

pv-wrap: Configure PulseAudio as default in /etc/asound.conf
This fixes two separate issues:

* It's parsed by all known versions of libasound.so.2, so it will work
  for both Steam Runtime 1 'scout' and Steam Runtime 2 'soldier', as
  well as future runtimes. Previously, we were using a path that only
  works in runtimes newer than scout.
* It's a direct child of /etc, which in our case is a tmpfs, so we
  don't need to worry about whether we will be able to create a mount
  point in a subdirectory.

Fixes: 1b720eb7 "wrap: Set PulseAudio as the default ALSA driver if available"
Signed-off-by: Simon McVittie <smcv@collabora.com>

pressure-vessel/runtime.c
@@ -2203,6 +2203,7 @@ bind_runtime_base (PvRuntime *self,
   };
   static const char * const dont_bind[] =
   {
+    "/etc/asound.conf",
     "/etc/localtime",
     "/etc/machine-id",
     "/var/cache/ldconfig",

pressure-vessel/wrap.c
@@ -2428,9 +2428,9 @@ main (int argc,
       const gchar *alsa_config = "pcm.!default pulse\n"
                                  "ctl.!default pulse\n";
 
-      flatpak_bwrap_add_args_data (bwrap, "99-pulseaudio-default.conf",
+      flatpak_bwrap_add_args_data (bwrap, "asound.conf",
                                    alsa_config, -1,
-                                   "/etc/alsa/conf.d/99-pulseaudio-default.conf",
+                                   "/etc/asound.conf",
                                    NULL);
     }
 
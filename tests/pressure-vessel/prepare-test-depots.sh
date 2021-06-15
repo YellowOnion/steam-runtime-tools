b6eaf062e3bcea01cbb7179c892de6a37a50a1e8
Simon McVittie <smcv@collabora.com>
2021-06-15 16:16:00 +0100

    tests: Don't rely on being able to put more than one suite in a depot
    The SteamLinuxRuntime scripts have phased out support for populating a
    depot directory with a mixture of scout and soldier, because that
    doesn't really make sense for a game or a compat tool: each native Linux
    game, and each compat tool used to run non-native games, expects to run
    in one particular environment (scout or soldier or something else) and
    using any other environment would be incorrect.
    
    However, tests/pressure-vessel/containers.py currently expects to work
    with a directory that contains both scout and soldier. For now, we'll
    download scout and soldier separately, then mash them together into the
    same directory.
    
    Signed-off-by: Simon McVittie <smcv@collabora.com>

---
+++ tests/pressure-vessel/prepare-test-depots.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+set -eux
+
+builddir="${1:-_build}"
+PRESSURE_VESSEL="${2:-_build/production/pressure-vessel-bin.tar.gz}"
+
+rm -fr "$builddir/depot"
+rm -fr "$builddir/depot-template"
+mkdir -p "$builddir/depot"
+mkdir -p "$builddir/depot-template/common"
+
+if [ -n "${IMAGES_DOWNLOAD_URL-}" ] && [ -n "${IMAGES_DOWNLOAD_CREDENTIAL-}" ]; then
+    suites="scout soldier"
+    set -- \
+        --include-sdk \
+        --credential-env IMAGES_DOWNLOAD_CREDENTIAL \
+        --images-uri "${IMAGES_DOWNLOAD_URL}"/steamrt-SUITE/snapshots \
+        ${NULL+}
+else
+    suites="scout"
+    set -- \
+        --version latest-steam-client-public-beta \
+        ${NULL+}
+fi
+
+for suite in $suites; do
+    python3 ./pressure-vessel/populate-depot.py \
+        --depot="$builddir/depots/$suite" \
+        --pressure-vessel "${PRESSURE_VESSEL}" \
+        --source-dir="$builddir/depot-template" \
+        --unpack-runtimes \
+        "$@" \
+        "${suite}"
+    for member in "$builddir/depots/$suite"/*; do
+        rm -fr "$builddir/depot/${member##*/}"
+    done
+    mv "$builddir/depots/$suite"/* "$builddir/depot/"
+done
+
+# vim:set sw=4 sts=4 et:

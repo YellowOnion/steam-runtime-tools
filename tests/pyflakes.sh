0cd9ae5a45021306570f8260f4b6e9c5edcd078b
Simon McVittie <smcv@collabora.com>
2021-08-17 13:06:24 +0100

    build-aux: Move sysroot convenience scripts to here
    Signed-off-by: Simon McVittie <smcv@collabora.com>

--- tests/pyflakes.sh
+++ tests/pyflakes.sh
@@ -37,9 +37,9 @@ if [ "x${PYFLAKES:=pyflakes3}" = xfalse ] || \
         [ -z "$(command -v "$PYFLAKES")" ]; then
     echo "1..0 # SKIP pyflakes3 not found"
 elif "${PYFLAKES}" \
+    ./build-aux/*.py \
     ./pressure-vessel/*.py \
     ./pressure-vessel/pressure-vessel-test-ui \
-    ./sysroot/*.py \
     ./tests/*/*.py \
     >&2; then
     echo "1..1"

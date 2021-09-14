a23cb089e1a5626e5109e734a71a019a487492ad
Ludovico de Nittis <ludovico.denittis@collabora.com>
2021-09-14 17:09:46 +0200

    runtime: Use correct ld.so from provider
    When we need to take the ld.so from the provider, we expect to have a
    path relative to the provider.
    
    Signed-off-by: Ludovico de Nittis <ludovico.denittis@collabora.com>

--- pressure-vessel/runtime.c
+++ pressure-vessel/runtime.c
@@ -3561,7 +3561,6 @@ pv_runtime_take_ld_so_from_provider (PvRuntime *self,
 {
   glnx_autofd int path_fd = -1;
   g_autofree gchar *ld_so_relative_to_provider = NULL;
-  g_autofree gchar *ld_so_in_provider = NULL;
 
   g_return_val_if_fail (self->provider != NULL, FALSE);
   g_return_val_if_fail (bwrap != NULL || self->mutable_sysroot != NULL, FALSE);
@@ -3579,9 +3578,6 @@ pv_runtime_take_ld_so_from_provider (PvRuntime *self,
       return FALSE;
     }
 
-  ld_so_in_provider = g_build_filename (self->provider->path_in_host_ns,
-                                        ld_so_relative_to_provider, NULL);
-
   g_debug ("Provider path: %s -> %s", arch->ld_so, ld_so_relative_to_provider);
   /* Might be either absolute, or relative to the root */
   g_debug ("Container path: %s -> %s", arch->ld_so, ld_so_in_runtime);
@@ -3593,7 +3589,7 @@ pv_runtime_take_ld_so_from_provider (PvRuntime *self,
    * having to dereference a long chain of symlinks every time we run
    * an executable. */
   if (self->mutable_sysroot != NULL &&
-      !pv_runtime_take_from_provider (self, bwrap, ld_so_in_provider,
+      !pv_runtime_take_from_provider (self, bwrap, ld_so_relative_to_provider,
                                       arch->ld_so,
                                       TAKE_FROM_PROVIDER_FLAGS_NONE, error))
     return FALSE;
@@ -3612,7 +3608,7 @@ pv_runtime_take_ld_so_from_provider (PvRuntime *self,
    * non-standard path), that's pretty much a disaster, because it will
    * just crash. However, all of those (chains of) non-standard symlinks
    * will end up pointing to ld_so_in_runtime. */
-  return pv_runtime_take_from_provider (self, bwrap, ld_so_in_provider,
+  return pv_runtime_take_from_provider (self, bwrap, ld_so_relative_to_provider,
                                         ld_so_in_runtime,
                                         TAKE_FROM_PROVIDER_FLAGS_NONE, error);
 }

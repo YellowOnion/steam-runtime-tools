YellowOnion/steam-runtime-tools

commit 70e9947aacde68e349dc85fc8f82092e1ef6ed86

Remove python-vdf subproject
python-vdf is now available in most of our CI images, so we don't need
it here. For test:bionic and test:archlinux, clone a known-good copy
from git and use that.

Signed-off-by: Simon McVittie <smcv@collabora.com>

populate-depot.py
@@ -64,18 +64,6 @@ from debian.deb822 import (
 HERE = os.path.dirname(os.path.abspath(__file__))
 
 
-# git remote add --no-tags python-vdf https://github.com/ValvePython/vdf
-# Update with:
-# git subtree merge -P subprojects/python-vdf python-vdf/master
-sys.path[:0] = [
-    os.path.join(
-        HERE,
-        'subprojects',
-        'python-vdf'
-    ),
-]
-
-
 logger = logging.getLogger('populate-depot')
 
 

.gitlab-ci.yml
@@ -62,15 +62,20 @@ test:bionic:
     image: ubuntu:18.04
     before_script:
         - |
+            set -eux
             export DEBIAN_FRONTEND=noninteractive
             apt-get -y update
             apt-get -y install \
                 ca-certificates \
+                git \
                 make \
                 perl \
                 python3 \
                 python3-debian \
                 ${NULL+}
+            tempdir="$(mktemp -d)"
+            git clone --branch steamrt/soldier https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.steamos.cloud/packaging/python-vdf.git "$tempdir/python-vdf"
+            export PYTHONPATH="$tempdir/python-vdf"
     script: make check
     artifacts:
         paths:
@@ -97,8 +102,9 @@ test:archlinux:
             ${NULL+}
             tempdir="$(mktemp -d)"
             git clone --branch debian/buster https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.steamos.cloud/packaging/python-debian.git "$tempdir/python-debian"
+            git clone --branch steamrt/soldier https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.steamos.cloud/packaging/python-vdf.git "$tempdir/python-vdf"
             export PATH="/usr/bin/core_perl:$PATH"
-            export PYTHONPATH="$tempdir/python-debian/lib"
+            export PYTHONPATH="$tempdir/python-debian/lib:$tempdir/python-vdf"
     script: make check
     artifacts:
         paths:

.reuse/dep5
@@ -15,12 +15,6 @@ Copyright:
 License: MIT
 
 Files:
- subprojects/python-vdf/*
-Copyright:
- 2015-2020 Rossen Georgiev
-License: MIT
-
-Files:
  tests/third-party/pycotap.py
 Copyright:
  2015 Remko Tronçon

subprojects/python-vdf/.gitignore
@@ -1,5 +1,0 @@
-dist
-*.egg-info
-*.pyc
-.coverage
-*.swp

subprojects/python-vdf/.scrutinizer.yml
@@ -1,17 +1,0 @@
-filter:
-    excluded_paths:
-        - 'tests/*'
-        - 'vdf2json/*'
-tools:
-    external_code_coverage:
-        timeout: 200
-        runs: 8
-
-build:
-  nodes:
-    analysis:
-      tests:
-        override:
-          - command: py-scrutinizer-run
-            idle_timeout: 300
-

subprojects/python-vdf/.travis.yml
@@ -1,24 +1,0 @@
-language: python
-os: linux
-
-jobs:
-  include:
-    - python: 2.7
-    - python: 3.4
-    - python: 3.5
-    - python: 3.6
-    - python: 3.7
-    - python: 3.8
-    - python: "pypy"
-    - python: "pypy3"
-install:
-    - pip install -r requirements.txt
-    - pip install coveralls
-    - pip install scrutinizer-ocular
-before_script:
-    - rm -f .coverage vdf/*.pyc tests/*.pyc
-script:
-    - PYTHONHASHSEED=0 python -m pytest --cov=vdf tests
-after_success:
-    - coveralls
-    - ocular --data-file ".coverage"

subprojects/python-vdf/LICENSE
@@ -1,19 +1,0 @@
-Copyright (c) 2015 Rossen Georgiev <rossen@rgp.io>
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-of the Software, and to permit persons to whom the Software is furnished to do
-so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.

subprojects/python-vdf/Makefile
@@ -1,41 +1,0 @@
-# Makefile for VDF module
-
-define HELPBODY
-Available commands:
-
-	make help       - this thing.
-	make init       - install python dependancies
-	make test       - run tests and coverage
-	make pylint     - code analysis
-	make build      - pylint + test
-
-endef
-
-export HELPBODY
-help:
-	@echo "$$HELPBODY"
-
-init:
-	pip install -r requirements.txt
-
-test:
-	rm -f .coverage vdf/*.pyc tests/*.pyc
-	PYTHONHASHSEED=0 python -m pytest --cov=vdf tests
-
-pylint:
-	pylint -r n -f colorized vdf || true
-
-build: pylint test
-
-clean:
-	rm -rf dist vdf.egg-info vdf/*.pyc
-
-dist: clean
-	python setup.py sdist
-	python setup.py bdist_wheel --universal
-
-register:
-	python setup.py register -r pypi
-
-upload: dist register
-	twine upload -r pypi dist/*

commit 2657996eefbc0ff273ad6d9babc3638d78ec3dd3

populate-depot: Drop now-unused import of sys
Fixes: 70e9947a "Remove python-vdf subproject"
Signed-off-by: Simon McVittie <smcv@collabora.com>

populate-depot.py
@@ -40,7 +40,6 @@ import shlex
 import shutil
 import stat
 import subprocess
-import sys
 import tarfile
 import tempfile
 import urllib.parse
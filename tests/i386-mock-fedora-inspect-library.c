77fc6f366a53ed0c90e00457ff6b4764d549c642
Ludovico de Nittis <ludovico.denittis@collabora.com>
2021-09-29 17:33:31 +0200

    tests: Simplify the mock fedora inspect libray
    Signed-off-by: Ludovico de Nittis <ludovico.denittis@collabora.com>

--- tests/i386-mock-fedora-inspect-library.c
+++ tests/i386-mock-fedora-inspect-library.c
@@ -35,41 +35,23 @@ main (int argc,
   g_return_val_if_fail (argc == 3, EXIT_FAILURE);
   g_return_val_if_fail (strcmp (argv[1], "--line-based") == 0, EXIT_FAILURE);
 
-  /* If the argument is an absolute path we assume it is a library loader.
+  /* We assume the argument to be a path for a library loader.
    * Because the loaders are mock objects we just check if they are located in
    * the expected locations. */
-  if (g_str_has_prefix (argv[2], "/"))
+  if (g_strstr_len (argv[2], -1, "/lib/i386-linux-gnu/") != NULL ||
+      g_strstr_len (argv[2], -1, "/lib32/dri/") != NULL ||
+      g_strstr_len (argv[2], -1, "/lib/dri/") != NULL ||
+      g_strstr_len (argv[2], -1, "/lib/vdpau/") != NULL ||
+      g_strstr_len (argv[2], -1, "/another_custom_path/") != NULL ||
+      g_strstr_len (argv[2], -1, "/custom_path32/") != NULL ||
+      g_strstr_len (argv[2], -1, "/custom_path32_2/") != NULL)
     {
-      if (g_strstr_len (argv[2], -1, "/lib/i386-linux-gnu/") != NULL ||
-          g_strstr_len (argv[2], -1, "/lib32/dri/") != NULL ||
-          g_strstr_len (argv[2], -1, "/lib/dri/") != NULL ||
-          g_strstr_len (argv[2], -1, "/lib/vdpau/") != NULL ||
-          g_strstr_len (argv[2], -1, "/another_custom_path/") != NULL ||
-          g_strstr_len (argv[2], -1, "/custom_path32/") != NULL ||
-          g_strstr_len (argv[2], -1, "/custom_path32_2/") != NULL)
-        {
-          printf ("requested=%s\n", argv[2]);
-          printf ("path=%s\n", argv[2]);
-          return EXIT_SUCCESS;
-        }
-      else
-        {
-          return EXIT_FAILURE;
-        }
+      printf ("requested=%s\n", argv[2]);
+      printf ("path=%s\n", argv[2]);
+      return EXIT_SUCCESS;
     }
 
-  /* If the argument is a 64bit directory, we return an exit failure */
-  if (g_strstr_len (argv[2], -1, "/custom_path64/") != NULL)
-    return EXIT_FAILURE;
-
-  gchar **envp = g_get_environ ();
-  gchar *path = g_build_filename (g_environ_getenv (envp, "SRT_TEST_SYSROOT"), "usr", "lib", argv[2], NULL);
-
-  /* Return as though we found the given soname in a canonical Fedora style,
-   * 32-bit lib directory */
-  printf ("requested=%s\n", argv[2]);
-  printf ("path=%s\n", path);
-  g_free (path);
-  g_strfreev (envp);
-  return EXIT_SUCCESS;
+  /* In all the other cases, e.g. if a 64bit directory was requested, we return
+   * an exit failure */
+  return EXIT_FAILURE;
 }

YellowOnion/steam-runtime-tools

commit a23cb089e1a5626e5109e734a71a019a487492ad

runtime: Use correct ld.so from provider
When we need to take the ld.so from the provider, we expect to have a
path relative to the provider.

Signed-off-by: Ludovico de Nittis <ludovico.denittis@collabora.com>

pressure-vessel/runtime.c
@@ -3561,7 +3561,6 @@ pv_runtime_take_ld_so_from_provider (PvRuntime *self,
 {
   glnx_autofd int path_fd = -1;
   g_autofree gchar *ld_so_relative_to_provider = NULL;
-  g_autofree gchar *ld_so_in_provider = NULL;
 
   g_return_val_if_fail (self->provider != NULL, FALSE);
   g_return_val_if_fail (bwrap != NULL || self->mutable_sysroot != NULL, FALSE);
@@ -3579,9 +3578,6 @@ pv_runtime_take_ld_so_from_provider (PvRuntime *self,
       return FALSE;
     }
 
-  ld_so_in_provider = g_build_filename (self->provider->path_in_host_ns,
-                                        ld_so_relative_to_provider, NULL);
-
   g_debug ("Provider path: %s -> %s", arch->ld_so, ld_so_relative_to_provider);
   /* Might be either absolute, or relative to the root */
   g_debug ("Container path: %s -> %s", arch->ld_so, ld_so_in_runtime);
@@ -3593,7 +3589,7 @@ pv_runtime_take_ld_so_from_provider (PvRuntime *self,
    * having to dereference a long chain of symlinks every time we run
    * an executable. */
   if (self->mutable_sysroot != NULL &&
-      !pv_runtime_take_from_provider (self, bwrap, ld_so_in_provider,
+      !pv_runtime_take_from_provider (self, bwrap, ld_so_relative_to_provider,
                                       arch->ld_so,
                                       TAKE_FROM_PROVIDER_FLAGS_NONE, error))
     return FALSE;
@@ -3612,7 +3608,7 @@ pv_runtime_take_ld_so_from_provider (PvRuntime *self,
    * non-standard path), that's pretty much a disaster, because it will
    * just crash. However, all of those (chains of) non-standard symlinks
    * will end up pointing to ld_so_in_runtime. */
-  return pv_runtime_take_from_provider (self, bwrap, ld_so_in_provider,
+  return pv_runtime_take_from_provider (self, bwrap, ld_so_relative_to_provider,
                                         ld_so_in_runtime,
                                         TAKE_FROM_PROVIDER_FLAGS_NONE, error);
 }

commit a88d5e97fca751247a575ec9e5b420da5151404a

pv-runtime: Handle root-relative paths when not copying runtime
Two recent branches independently changed bits of PvRuntime for better
correctness in corner cases, and ended up passing in relative paths,
intending them to be interpreted as relative to the top of the sysroot
(provider or final container, as appropriate). This worked OK for the
code path where we modify a mutable copy of the sysroot in-place, but
did not work for the code path where we build up a long bwrap
command-line.

Fixes: a23cb089 "runtime: Use correct ld.so from provider"
Fixes: 485269b6 "pv-runtime: Use _srt_get_path_after to remove provider prefix"
Signed-off-by: Simon McVittie <smcv@collabora.com>

pressure-vessel/runtime.c
@@ -3011,6 +3011,23 @@ typedef enum
   TAKE_FROM_PROVIDER_FLAGS_NONE = 0
 } TakeFromProviderFlags;
 
+/*
+ * pv_runtime_take_from_provider:
+ * @self: the runtime
+ * @bwrap: bubblewrap arguments
+ * @source_in_provider: source path in the graphics stack provider's
+ *  namespace, either absolute or relative to the root
+ * @dest_in_container: destination path in the container we are creating,
+ *  either absolute or relative to the root
+ * @flags: flags affecting how we do it
+ * @error: used to report error
+ *
+ * Try to arrange for @source_in_provider to be made available at the
+ * path @dest_in_container in the container we are creating.
+ *
+ * Note that neither @source_in_provider nor @dest_in_container is
+ * guaranteed to be an absolute path.
+ */
 static gboolean
 pv_runtime_take_from_provider (PvRuntime *self,
                                FlatpakBwrap *bwrap,
@@ -3148,29 +3165,54 @@ pv_runtime_take_from_provider (PvRuntime *self,
     }
   else
     {
+      g_autofree gchar *source_in_current_ns = NULL;
+      g_autofree gchar *realpath_in_provider = NULL;
+      g_autofree gchar *abs_dest = NULL;
+      glnx_autofd int source_fd = -1;
+
       /* We can't edit the runtime in-place, so tell bubblewrap to mount
        * a new version over the top */
       g_assert (bwrap != NULL);
 
+      source_fd = _srt_resolve_in_sysroot (self->provider->fd,
+                                           source_in_provider,
+                                           SRT_RESOLVE_FLAGS_NONE,
+                                           &realpath_in_provider, error);
+
+      if (source_fd < 0)
+        return FALSE;
+
       if (flags & TAKE_FROM_PROVIDER_FLAGS_IF_CONTAINER_COMPATIBLE)
         {
           g_autofree gchar *dest = NULL;
+          struct stat stat_buf;
 
           if (g_str_has_prefix (dest_in_container, "/usr/"))
             dest = g_build_filename (self->runtime_usr,
                                      dest_in_container + strlen ("/usr/"),
+                                     NULL);
+          else if (g_str_has_prefix (dest_in_container, "usr/"))
+            dest = g_build_filename (self->runtime_usr,
+                                     dest_in_container + strlen ("usr/"),
                                      NULL);
           else
             dest = g_build_filename (self->runtime_files,
                                      dest_in_container,
                                      NULL);
 
-          if (g_file_test (source_in_provider, G_FILE_TEST_IS_DIR))
+          if (fstat (source_fd, &stat_buf) != 0)
+            return glnx_throw_errno_prefix (error,
+                                            "fstat \"%s/%s\"",
+                                            self->provider->path_in_current_ns,
+                                            realpath_in_provider);
+
+          if (S_ISDIR (stat_buf.st_mode))
             {
               if (!g_file_test (dest, G_FILE_TEST_IS_DIR))
                 {
-                  g_warning ("Not mounting \"%s\" over non-directory file or "
-                             "nonexistent path \"%s\"",
+                  g_warning ("Not mounting \"%s/%s\" over "
+                             "non-directory file or nonexistent path \"%s\"",
+                             self->provider->path_in_current_ns,
                              source_in_provider, dest);
                   return TRUE;
                 }
@@ -3180,16 +3222,23 @@ pv_runtime_take_from_provider (PvRuntime *self,
               if (g_file_test (dest, G_FILE_TEST_IS_DIR) ||
                   !g_file_test (dest, G_FILE_TEST_EXISTS))
                 {
-                  g_warning ("Not mounting \"%s\" over directory or "
+                  g_warning ("Not mounting \"%s/%s\" over directory or "
                              "nonexistent path \"%s\"",
+                             self->provider->path_in_current_ns,
                              source_in_provider, dest);
                   return TRUE;
                 }
             }
         }
 
+      /* This is not 100% robust against the provider sysroot being
+       * modified while we're looking at it, but it's the best we can do. */
+      source_in_current_ns = g_build_filename (self->provider->path_in_current_ns,
+                                               realpath_in_provider,
+                                               NULL);
+      abs_dest = g_build_filename ("/", dest_in_container, NULL);
       flatpak_bwrap_add_args (bwrap,
-                              "--ro-bind", source_in_provider, dest_in_container,
+                              "--ro-bind", source_in_current_ns, abs_dest,
                               NULL);
     }
 
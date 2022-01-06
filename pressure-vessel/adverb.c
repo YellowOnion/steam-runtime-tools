YellowOnion/steam-runtime-tools

commit b3f5390a591bc669193adb5d42598f98a143ae58

pv-adverb: Avoid jumping past initialization
When we add proper error-checking for determining the path to
/proc/self/exe, which will be done in a subsequent commit, it will
involve a 'goto out' that jumps past initialization of this variable.

Signed-off-by: Simon McVittie <smcv@collabora.com>

pressure-vessel/adverb.c
@@ -778,6 +778,13 @@ generate_locales (gchar **locpath_out,
   g_autofree gchar *this_path = NULL;
   g_autofree gchar *this_dir = NULL;
   gboolean ret = FALSE;
+  const char *locale_gen_argv[] =
+  {
+    NULL,   /* placeholder for /path/to/pressure-vessel-locale-gen */
+    "--output-dir", temp_dir,
+    "--verbose",
+    NULL
+  };
 
   g_return_val_if_fail (locpath_out != NULL && *locpath_out == NULL, FALSE);
   g_return_val_if_fail (error == NULL || *error == NULL, FALSE);
@@ -785,16 +792,9 @@ generate_locales (gchar **locpath_out,
   this_path = g_file_read_link ("/proc/self/exe", NULL);
   this_dir = g_path_get_dirname (this_path);
   pvlg = g_build_filename (this_dir, "pressure-vessel-locale-gen", NULL);
+  locale_gen_argv[0] = pvlg;
 
   temp_dir = g_dir_make_tmp ("pressure-vessel-locales-XXXXXX", error);
-
-  const char * const locale_gen_argv[] =
-  {
-    pvlg,
-    "--output-dir", temp_dir,
-    "--verbose",
-    NULL
-  };
 
   if (temp_dir == NULL)
     {

commit 3991c96907aeddf08275ef41e3e83656f8ab2cea

pv-adverb: Fix regression in generation of required locales
Fixes: b3f5390a "pv-adverb: Avoid jumping past initialization"
Signed-off-by: Simon McVittie <smcv@collabora.com>

pressure-vessel/adverb.c
@@ -780,7 +780,7 @@ generate_locales (gchar **locpath_out,
   const char *locale_gen_argv[] =
   {
     NULL,   /* placeholder for /path/to/pressure-vessel-locale-gen */
-    "--output-dir", temp_dir,
+    "--output-dir", NULL,
     "--verbose",
     NULL
   };
@@ -797,6 +797,7 @@ generate_locales (gchar **locpath_out,
   locale_gen_argv[0] = pvlg;
 
   temp_dir = g_dir_make_tmp ("pressure-vessel-locales-XXXXXX", error);
+  locale_gen_argv[2] = temp_dir;
 
   if (temp_dir == NULL)
     {
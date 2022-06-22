YellowOnion/steam-runtime-tools

commit 2194b59a6c4150926bb5250fc0f48f9f3ec751a4

Move pressure-vessel's logging setup into library code
The names were confusingly similar, but _srt_util_set_up_logging() was
actually orthogonal to pv_set_up_logging(): the former was about the
disposition of the stdout and stderr fds, whereas the latter was about
the GLib log handler. Rename it to make the difference clearer.

Signed-off-by: Simon McVittie <smcv@collabora.com>

pressure-vessel/adverb.c
@@ -35,6 +35,7 @@
 #include <gio/gio.h>
 
 #include "steam-runtime-tools/glib-backports-internal.h"
+#include "steam-runtime-tools/log-internal.h"
 #include "steam-runtime-tools/profiling-internal.h"
 #include "steam-runtime-tools/steam-runtime-tools.h"
 #include "steam-runtime-tools/utils-internal.h"
@@ -1073,7 +1074,7 @@ main (int argc,
   g_set_prgname ("pressure-vessel-adverb");
 
   /* Set up the initial base logging */
-  pv_set_up_logging (FALSE);
+  _srt_util_set_glib_log_handler (FALSE);
 
   context = g_option_context_new (
       "COMMAND [ARG...]\n"
@@ -1107,7 +1108,7 @@ main (int argc,
     }
 
   if (opt_verbose)
-    pv_set_up_logging (opt_verbose);
+    _srt_util_set_glib_log_handler (opt_verbose);
 
   original_stdout = _srt_divert_stdout_to_stderr (error);
 
@@ -1523,7 +1524,7 @@ out:
   g_clear_pointer (&opt_preload_modules, g_array_unref);
 
   if (local_error != NULL)
-    pv_log_failure ("%s", local_error->message);
+    _srt_log_failure ("%s", local_error->message);
 
   global_original_environ = NULL;
   g_debug ("Exiting with status %d", ret);

pressure-vessel/wrap.c
@@ -31,6 +31,7 @@
 #include <string.h>
 
 #include "steam-runtime-tools/glib-backports-internal.h"
+#include "steam-runtime-tools/log-internal.h"
 #include "steam-runtime-tools/profiling-internal.h"
 #include "steam-runtime-tools/utils-internal.h"
 #include "libglnx/libglnx.h"
@@ -1159,7 +1160,7 @@ tristate_environment (const gchar *name)
   return TRISTATE_MAYBE;
 }
 
-#define usage_error(...) pv_log_failure (__VA_ARGS__)
+#define usage_error(...) _srt_log_failure (__VA_ARGS__)
 
 int
 main (int argc,
@@ -1202,7 +1203,7 @@ main (int argc,
   g_set_prgname ("pressure-vessel-wrap");
 
   /* Set up the initial base logging */
-  pv_set_up_logging (FALSE);
+  _srt_util_set_glib_log_handler (FALSE);
 
   g_info ("pressure-vessel version %s", VERSION);
 
@@ -1288,7 +1289,7 @@ main (int argc,
     goto out;
 
   if (opt_verbose)
-    pv_set_up_logging (opt_verbose);
+    _srt_util_set_glib_log_handler (opt_verbose);
 
   /* Specifying either one of these mutually-exclusive options as a
    * command-line option disables use of the environment variable for
@@ -2550,7 +2551,7 @@ main (int argc,
 
 out:
   if (local_error != NULL)
-    pv_log_failure ("%s", local_error->message);
+    _srt_log_failure ("%s", local_error->message);
 
   g_clear_pointer (&opt_preload_modules, g_array_unref);
   g_clear_pointer (&adverb_preload_argv, g_ptr_array_unref);

steam-runtime-tools/log-internal.h
@@ -30,4 +30,10 @@
 
 #include <steam-runtime-tools/macros.h>
 
+#define SRT_LOG_LEVEL_FAILURE (1 << G_LOG_LEVEL_USER_SHIFT)
+
+#define _srt_log_failure(...) \
+  g_log (G_LOG_DOMAIN, SRT_LOG_LEVEL_FAILURE, __VA_ARGS__)
+
+void _srt_util_set_glib_log_handler (gboolean opt_verbose);
 void _srt_util_set_up_logging (const char *identifier);

steam-runtime-tools/log.c
@@ -33,7 +33,39 @@
 
 #include <libglnx.h>
 
+#include "steam-runtime-tools/profiling-internal.h"
 #include "steam-runtime-tools/utils-internal.h"
+
+/*
+ * SRT_LOG_LEVEL_FAILURE:
+ *
+ * A log level for logging fatal errors that do not indicate a programming
+ * error, for example an invalid command-line option, or being asked to
+ * open a file that does not exist.
+ *
+ * Only use this in programs that have called
+ * _srt_util_set_glib_log_handler().
+ *
+ * This is functionally equivalent to %G_LOG_LEVEL_MESSAGE, but
+ * the handler set up by _srt_util_set_glib_log_handler() prints it
+ * as though it was a fatal error. Use it in command-line utilities
+ * to log a GError that will cause program termination.
+ *
+ * Unlike %G_LOG_LEVEL_ERROR, this is not considered to indicate a
+ * programming error, and does not cause a core dump.
+ */
+
+/*
+ * _srt_log_failure:
+ * @...: format string and arguments, as for g_message()
+ *
+ * Convenience macro to log at level %SRT_LOG_LEVEL_FAILURE.
+ * Only use this in programs that have called
+ * _srt_util_set_glib_log_handler().
+ *
+ * This is functionally equivalent to g_message(), but with a different
+ * log level.
+ */
 
 typedef void *AutoLibraryHandle;
 G_DEFINE_AUTO_CLEANUP_FREE_FUNC (AutoLibraryHandle, dlclose, NULL);
@@ -199,4 +231,132 @@ _srt_util_set_up_logging (const char *identifier)
       if (!_srt_stdio_to_journal (identifier, STDERR_FILENO, LOG_NOTICE, &local_error))
         g_warning ("%s: %s", identifier, local_error->message);
     }
+}
+
+static int my_pid = -1;
+static const gchar *my_prgname = NULL;
+
+/*
+ * get_level_prefix:
+ * @log_level: A GLib log level, which should normally only have one bit set
+ *
+ * Returns: A short prefix for log messages, for example "W" for warnings
+ *  or "Internal error" for assertion failures.
+ */
+static const char *
+get_level_prefix (GLogLevelFlags log_level)
+{
+  if (log_level & (G_LOG_FLAG_RECURSION
+                   | G_LOG_FLAG_FATAL
+                   | G_LOG_LEVEL_ERROR
+                   | G_LOG_LEVEL_CRITICAL))
+    return "Internal error";
+
+  if (log_level & SRT_LOG_LEVEL_FAILURE)
+    return "E";
+
+  if (log_level & G_LOG_LEVEL_WARNING)
+    return "W";
+
+  if (log_level & G_LOG_LEVEL_MESSAGE)
+    return "N";   /* consistent with apt, which calls this a "notice" */
+
+  if (log_level & G_LOG_LEVEL_INFO)
+    return "I";
+
+  if (log_level & G_LOG_LEVEL_DEBUG)
+    return "D";
+
+  return "?!";
+}
+
+/*
+ * log_to_stderr_with_timestamp:
+ * @log_domain: the log domain of the message
+ * @log_level: the log level of the message
+ * @message: the message to process
+ * @user_data: not used
+ */
+static void
+log_to_stderr_with_timestamp (const gchar *log_domain,
+                              GLogLevelFlags log_level,
+                              const gchar *message,
+                              gpointer user_data)
+{
+  g_autoptr(GDateTime) date_time = g_date_time_new_now_local ();
+
+  /* We can't use the format specifier "%f" for microseconds because it
+   * was introduced in GLib 2.66 and we are targeting an older version */
+  g_autofree gchar *timestamp = g_date_time_format (date_time, "%T");
+
+  g_printerr ("%s.%06i: %s[%d]: %s: %s\n", timestamp,
+              g_date_time_get_microsecond (date_time),
+              my_prgname, my_pid, get_level_prefix (log_level), message);
+
+  if (log_level & (G_LOG_FLAG_RECURSION
+                   | G_LOG_FLAG_FATAL
+                   | G_LOG_LEVEL_ERROR))
+    G_BREAKPOINT ();
+}
+
+/*
+ * log_to_stderr:
+ * @log_domain: the log domain of the message
+ * @log_level: the log level of the message
+ * @message: the message to process
+ * @user_data: not used
+ */
+static void
+log_to_stderr (const gchar *log_domain,
+               GLogLevelFlags log_level,
+               const gchar *message,
+               gpointer user_data)
+{
+  g_printerr ("%s[%d]: %s: %s\n",
+              my_prgname, my_pid, get_level_prefix (log_level), message);
+
+  if (log_level & (G_LOG_FLAG_RECURSION
+                   | G_LOG_FLAG_FATAL
+                   | G_LOG_LEVEL_ERROR))
+    G_BREAKPOINT ();
+}
+
+/*
+ * _srt_util_set_glib_log_handler:
+ * @opt_verbose: If %TRUE, enable g_debug() messages and profiling
+ *
+ * Configure GLib to log to stderr with a message format suitable for
+ * command-line programs, for example
+ *
+ * ```
+ * my-program[123]: W: Resonance cascade scenario occurred
+ * ```
+ */
+void
+_srt_util_set_glib_log_handler (gboolean opt_verbose)
+{
+  GLogLevelFlags log_levels = (G_LOG_LEVEL_ERROR
+                               | G_LOG_LEVEL_CRITICAL
+                               | SRT_LOG_LEVEL_FAILURE
+                               | G_LOG_LEVEL_WARNING
+                               | G_LOG_LEVEL_MESSAGE);
+  gboolean opt_timestamp = _srt_boolean_environment ("PRESSURE_VESSEL_LOG_WITH_TIMESTAMP",
+                                                     FALSE);
+  gboolean opt_info = _srt_boolean_environment ("PRESSURE_VESSEL_LOG_INFO", FALSE);
+
+  my_pid = getpid ();
+  my_prgname = g_get_prgname ();
+
+  if (opt_info)
+    log_levels |= G_LOG_LEVEL_INFO;
+
+  if (opt_verbose)
+    {
+      log_levels |= G_LOG_LEVEL_DEBUG | G_LOG_LEVEL_INFO;
+      _srt_profiling_enable ();
+    }
+
+  g_log_set_handler (G_LOG_DOMAIN, log_levels,
+                     opt_timestamp ? log_to_stderr_with_timestamp : log_to_stderr,
+                     NULL);
 }

pressure-vessel/launch.c
@@ -39,6 +39,7 @@
 #include <gio/gunixfdlist.h>
 
 #include "steam-runtime-tools/glib-backports-internal.h"
+#include "steam-runtime-tools/log-internal.h"
 #include "steam-runtime-tools/utils-internal.h"
 #include "libglnx/libglnx.h"
 
@@ -740,7 +741,7 @@ main (int argc,
   g_set_prgname ("pressure-vessel-launch");
 
   /* Set up the initial base logging */
-  pv_set_up_logging (FALSE);
+  _srt_util_set_glib_log_handler (FALSE);
 
   context = g_option_context_new ("COMMAND [ARG...]");
   g_option_context_set_summary (context,
@@ -766,7 +767,7 @@ main (int argc,
     }
 
   if (opt_verbose)
-    pv_set_up_logging (opt_verbose);
+    _srt_util_set_glib_log_handler (opt_verbose);
 
   original_stdout = _srt_divert_stdout_to_stderr (error);
 
@@ -1249,7 +1250,7 @@ main (int argc,
 
 out:
   if (local_error != NULL)
-    pv_log_failure ("%s", local_error->message);
+    _srt_log_failure ("%s", local_error->message);
 
   if (signal_source > 0)
     g_source_remove (signal_source);

pressure-vessel/launcher.c
@@ -41,6 +41,7 @@
 #include <gio/gunixfdlist.h>
 
 #include "steam-runtime-tools/glib-backports-internal.h"
+#include "steam-runtime-tools/log-internal.h"
 #include "steam-runtime-tools/utils-internal.h"
 #include "libglnx/libglnx.h"
 
@@ -552,7 +553,7 @@ on_bus_acquired (PvPortalListener *listener,
 
   if (!export_launcher (connection, &error))
     {
-      pv_log_failure ("Unable to export object: %s", error->message);
+      _srt_log_failure ("Unable to export object: %s", error->message);
       *ret = EX_SOFTWARE;
       g_main_loop_quit (main_loop);
     }
@@ -669,7 +670,7 @@ connect_to_signals (void)
 
   if (sfd < 0)
     {
-      pv_log_failure ("Unable to watch signals: %s", g_strerror (errno));
+      _srt_log_failure ("Unable to watch signals: %s", g_strerror (errno));
       return 0;
     }
 
@@ -853,7 +854,7 @@ main (int argc,
   g_set_prgname ("pressure-vessel-launcher");
 
   /* Set up the initial base logging */
-  pv_set_up_logging (FALSE);
+  _srt_util_set_glib_log_handler (FALSE);
 
   context = g_option_context_new ("");
   g_option_context_set_summary (context,
@@ -880,7 +881,7 @@ main (int argc,
     }
 
   if (opt_verbose)
-    pv_set_up_logging (opt_verbose);
+    _srt_util_set_glib_log_handler (opt_verbose);
 
   if ((result = _srt_set_compatible_resource_limits (0)) < 0)
     g_warning ("Unable to set normal resource limits: %s",
@@ -984,7 +985,7 @@ main (int argc,
 
 out:
   if (local_error != NULL)
-    pv_log_failure ("%s", local_error->message);
+    _srt_log_failure ("%s", local_error->message);
 
   if (exit_on_readable_id > 0)
     g_source_remove (exit_on_readable_id);

pressure-vessel/utils.c
@@ -43,9 +43,6 @@
 #include "flatpak-utils-base-private.h"
 #include "flatpak-utils-private.h"
 
-static int my_pid = -1;
-static const gchar *my_prgname = NULL;
-
 /**
  * pv_get_current_dirs:
  * @cwd_p: (out) (transfer full) (optional): Used to return the
@@ -795,117 +792,6 @@ pv_current_namespace_path_to_host_path (const gchar *current_env_path)
     path_on_host = g_strdup (current_env_path);
 
   return path_on_host;
-}
-
-static const char *
-get_level_prefix (GLogLevelFlags log_level)
-{
-  if (log_level & (G_LOG_FLAG_RECURSION
-                   | G_LOG_FLAG_FATAL
-                   | G_LOG_LEVEL_ERROR
-                   | G_LOG_LEVEL_CRITICAL))
-    return "Internal error";
-
-  if (log_level & PV_LOG_LEVEL_FAILURE)
-    return "E";
-
-  if (log_level & G_LOG_LEVEL_WARNING)
-    return "W";
-
-  if (log_level & G_LOG_LEVEL_MESSAGE)
-    return "N";   /* consistent with apt, which calls this a "notice" */
-
-  if (log_level & G_LOG_LEVEL_INFO)
-    return "I";
-
-  if (log_level & G_LOG_LEVEL_DEBUG)
-    return "D";
-
-  return "?!";
-}
-
-/**
- * log_handler_with_timestamp:
- * @log_domain: the log domain of the message
- * @log_level: the log level of the message
- * @message: the message to process
- * @user_data: not used
- */
-static void
-pv_log_to_stderr_with_timestamp (const gchar *log_domain,
-                                 GLogLevelFlags log_level,
-                                 const gchar *message,
-                                 gpointer user_data)
-{
-  g_autoptr(GDateTime) date_time = g_date_time_new_now_local ();
-
-  /* We can't use the format specifier "%f" for microseconds because it
-   * was introduced in GLib 2.66 and we are targeting an older version */
-  g_autofree gchar *timestamp = g_date_time_format (date_time, "%T");
-
-  g_printerr ("%s.%06i: %s[%d]: %s: %s\n", timestamp,
-              g_date_time_get_microsecond (date_time),
-              my_prgname, my_pid, get_level_prefix (log_level), message);
-
-  if (log_level & (G_LOG_FLAG_RECURSION
-                   | G_LOG_FLAG_FATAL
-                   | G_LOG_LEVEL_ERROR))
-    G_BREAKPOINT ();
-}
-
-/**
- * pv_log_to_stderr:
- * @log_domain: the log domain of the message
- * @log_level: the log level of the message
- * @message: the message to process
- * @user_data: not used
- */
-static void
-pv_log_to_stderr (const gchar *log_domain,
-                  GLogLevelFlags log_level,
-                  const gchar *message,
-                  gpointer user_data)
-{
-  g_printerr ("%s[%d]: %s: %s\n",
-              my_prgname, my_pid, get_level_prefix (log_level), message);
-
-  if (log_level & (G_LOG_FLAG_RECURSION
-                   | G_LOG_FLAG_FATAL
-                   | G_LOG_LEVEL_ERROR))
-    G_BREAKPOINT ();
-}
-
-/**
- * pv_set_up_logging:
- * @opt_verbose: If the log should be more verbose
- */
-void
-pv_set_up_logging (gboolean opt_verbose)
-{
-  GLogLevelFlags log_levels = (G_LOG_LEVEL_ERROR
-                               | G_LOG_LEVEL_CRITICAL
-                               | PV_LOG_LEVEL_FAILURE
-                               | G_LOG_LEVEL_WARNING
-                               | G_LOG_LEVEL_MESSAGE);
-  gboolean opt_timestamp = _srt_boolean_environment ("PRESSURE_VESSEL_LOG_WITH_TIMESTAMP",
-                                                     FALSE);
-  gboolean opt_info = _srt_boolean_environment ("PRESSURE_VESSEL_LOG_INFO", FALSE);
-
-  my_pid = getpid ();
-  my_prgname = g_get_prgname ();
-
-  if (opt_info)
-    log_levels |= G_LOG_LEVEL_INFO;
-
-  if (opt_verbose)
-    {
-      log_levels |= G_LOG_LEVEL_DEBUG | G_LOG_LEVEL_INFO;
-      _srt_profiling_enable ();
-    }
-
-  g_log_set_handler (G_LOG_DOMAIN, log_levels,
-                     opt_timestamp ? pv_log_to_stderr_with_timestamp : pv_log_to_stderr,
-                     NULL);
 }
 
 /**

pressure-vessel/utils.h
@@ -33,11 +33,6 @@
 #define DBUS_INTERFACE_DBUS DBUS_NAME_DBUS
 #define DBUS_PATH_DBUS "/org/freedesktop/DBus"
 
-#define PV_LOG_LEVEL_FAILURE (1 << G_LOG_LEVEL_USER_SHIFT)
-
-#define pv_log_failure(...) \
-  g_log (G_LOG_DOMAIN, PV_LOG_LEVEL_FAILURE, __VA_ARGS__)
-
 void pv_get_current_dirs (gchar **cwd_p,
                           gchar **cwd_l);
 
@@ -66,8 +61,6 @@ gboolean pv_terminate_all_child_processes (GTimeSpan wait_period,
                                            GError **error);
 
 gchar *pv_current_namespace_path_to_host_path (const gchar *current_env_path);
-
-void pv_set_up_logging (gboolean opt_verbose);
 
 void pv_delete_dangling_symlink (int dirfd,
                                  const char *debug_path,

commit 701471db738ac47da525391256a167499bdcd3b2

pressure-vessel: Override log handler for pressure-vessel log domain
When this code moved into the shared library, it wasn't adapted to
take into account pressure-vessel and steam-runtime-tools being separate
log domains. The result was that PRESSURE_VESSEL_VERBOSE actually only
made libsteam-runtime-tools verbose, and info/debug messages from the
pressure-vessel directory were lost.

Fixes: 2194b59 "Move pressure-vessel's logging setup into library code"
Signed-off-by: Simon McVittie <smcv@collabora.com>

bin/launch-client.c
@@ -821,7 +821,7 @@ main (int argc,
   g_set_prgname ("steam-runtime-launch-client");
 
   /* Set up the initial base logging */
-  _srt_util_set_glib_log_handler (FALSE);
+  _srt_util_set_glib_log_handler (G_LOG_DOMAIN, FALSE);
 
   context = g_option_context_new ("COMMAND [ARG...]");
   g_option_context_set_summary (context,
@@ -847,7 +847,7 @@ main (int argc,
     }
 
   if (opt_verbose)
-    _srt_util_set_glib_log_handler (opt_verbose);
+    _srt_util_set_glib_log_handler (G_LOG_DOMAIN, opt_verbose);
 
   if (opt_list)
     {

bin/launcher-service.c
@@ -1260,7 +1260,7 @@ main (int argc,
   g_set_prgname ("steam-runtime-launcher-service");
 
   /* Set up the initial base logging */
-  _srt_util_set_glib_log_handler (FALSE);
+  _srt_util_set_glib_log_handler (G_LOG_DOMAIN, FALSE);
 
   context = g_option_context_new ("");
   g_option_context_set_summary (context,
@@ -1301,7 +1301,7 @@ main (int argc,
     }
 
   if (opt_verbose)
-    _srt_util_set_glib_log_handler (opt_verbose);
+    _srt_util_set_glib_log_handler (G_LOG_DOMAIN, opt_verbose);
 
   if (opt_stop_on_exit)
     server->flags |= PV_LAUNCHER_SERVER_FLAGS_STOP_ON_EXIT;

pressure-vessel/adverb.c
@@ -990,7 +990,7 @@ main (int argc,
   g_set_prgname ("pressure-vessel-adverb");
 
   /* Set up the initial base logging */
-  _srt_util_set_glib_log_handler (FALSE);
+  _srt_util_set_glib_log_handler (G_LOG_DOMAIN, FALSE);
 
   context = g_option_context_new (
       "COMMAND [ARG...]\n"
@@ -1024,7 +1024,7 @@ main (int argc,
     }
 
   if (opt_verbose)
-    _srt_util_set_glib_log_handler (opt_verbose);
+    _srt_util_set_glib_log_handler (G_LOG_DOMAIN, opt_verbose);
 
   original_stdout = _srt_divert_stdout_to_stderr (error);
 

pressure-vessel/wrap.c
@@ -1001,7 +1001,7 @@ main (int argc,
   g_set_prgname ("pressure-vessel-wrap");
 
   /* Set up the initial base logging */
-  _srt_util_set_glib_log_handler (FALSE);
+  _srt_util_set_glib_log_handler (G_LOG_DOMAIN, FALSE);
 
   g_info ("pressure-vessel version %s", VERSION);
 
@@ -1087,7 +1087,7 @@ main (int argc,
     goto out;
 
   if (opt_verbose)
-    _srt_util_set_glib_log_handler (opt_verbose);
+    _srt_util_set_glib_log_handler (G_LOG_DOMAIN, opt_verbose);
 
   interpreter_root = pv_wrap_detect_interpreter_root ();
 

steam-runtime-tools/log-internal.h
@@ -35,5 +35,6 @@
 #define _srt_log_failure(...) \
   g_log (G_LOG_DOMAIN, SRT_LOG_LEVEL_FAILURE, __VA_ARGS__)
 
-void _srt_util_set_glib_log_handler (gboolean opt_verbose);
+void _srt_util_set_glib_log_handler (const char *extra_log_domain,
+                                     gboolean opt_verbose);
 void _srt_util_set_up_logging (const char *identifier);

steam-runtime-tools/log.c
@@ -323,6 +323,7 @@ log_to_stderr (const gchar *log_domain,
 
 /*
  * _srt_util_set_glib_log_handler:
+ * @extra_log_domain: (nullable): A log domain, usually %G_LOG_DOMAIN
  * @opt_verbose: If %TRUE, enable g_debug() messages and profiling
  *
  * Configure GLib to log to stderr with a message format suitable for
@@ -331,9 +332,14 @@ log_to_stderr (const gchar *log_domain,
  * ```
  * my-program[123]: W: Resonance cascade scenario occurred
  * ```
+ *
+ * The chosen message format is used for @extra_log_domain, and also
+ * for the `steam-runtime-tools` log domain used by the
+ * steam-runtime-tools library.
  */
 void
-_srt_util_set_glib_log_handler (gboolean opt_verbose)
+_srt_util_set_glib_log_handler (const char *extra_log_domain,
+                                gboolean opt_verbose)
 {
   GLogLevelFlags log_levels = (G_LOG_LEVEL_ERROR
                                | G_LOG_LEVEL_CRITICAL
@@ -356,6 +362,9 @@ _srt_util_set_glib_log_handler (gboolean opt_verbose)
       _srt_profiling_enable ();
     }
 
+  g_log_set_handler (extra_log_domain, log_levels,
+                     opt_timestamp ? log_to_stderr_with_timestamp : log_to_stderr,
+                     NULL);
   g_log_set_handler (G_LOG_DOMAIN, log_levels,
                      opt_timestamp ? log_to_stderr_with_timestamp : log_to_stderr,
                      NULL);
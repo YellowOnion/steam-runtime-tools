YellowOnion/steam-runtime-tools

commit 9b3741ad16263aace6bb9fbc92d6a5aa3f68e133

cpu-feature: Add support for CPUID subleaf functions
Unlike `__get_cpuid` there isn't a `__get_cpuid_count` in the version of
GCC used to compile. Rely on `__cpuid_count` exclusively.

steam-runtime-tools/cpu-feature-internal.h
@@ -85,6 +85,15 @@ gboolean _srt_x86_cpuid (GHashTable *mock_cpuid,
                          guint *ebx,
                          guint *ecx,
                          guint *edx);
+
+gboolean _srt_x86_cpuid_count (GHashTable *mock_cpuid,
+                               guint leaf,
+                               guint subleaf,
+                               guint *eax,
+                               guint *ebx,
+                               guint *ecx,
+                               guint *edx);
+
 #endif
 
 G_GNUC_INTERNAL

steam-runtime-tools/cpu-feature.c
@@ -104,6 +104,40 @@ gboolean _srt_x86_cpuid (GHashTable *mock_cpuid,
         }
     }
 }
+
+gboolean _srt_x86_cpuid_count (GHashTable *mock_cpuid,
+                               guint leaf,
+                               guint subleaf,
+                               guint *eax,
+                               guint *ebx,
+                               guint *ecx,
+                               guint *edx)
+{
+  if (G_UNLIKELY (mock_cpuid != NULL))
+    {
+      SrtCpuidData *mock = g_hash_table_lookup (mock_cpuid,
+                                                GUINT_TO_POINTER ((guint64)subleaf << 32 | leaf));
+
+      if (mock == NULL)
+        return FALSE;
+
+      *eax = mock->registers[0];
+      *ebx = mock->registers[1];
+      *ecx = mock->registers[2];
+      *edx = mock->registers[3];
+      return TRUE;
+    }
+  else
+    {
+      *eax = *ebx = *ecx = *edx = 0;
+
+      /* __cpuid takes lvalues as parameters, and takes their addresses
+       * internally, so this is right even though it looks wrong. */
+      __cpuid_count (leaf, subleaf, (*eax), (*ebx), (*ecx), (*edx));
+      return TRUE;
+    }
+}
+
 #endif
 
 /**

commit 27c56fc4ddeb8298604cc4fa9092b933bad787b0

cpu-feature: Use struct { leaf, subleaf } as key for mock data
Now that we handle leaves that require a specific sub-leaf in ECX,
a single pointer-sized key is not enough: on 32-bit x86, the leaf
is pointer-sized, leaving no space to encode a sub-leaf correctly.
Use a pointer to a struct as the key, with custom equals and hash
functions.

Fixes: 9b3741a "cpu-feature: Add support for CPUID subleaf functions"
Resolves: steam-runtime-tools#94
Signed-off-by: Simon McVittie <smcv@collabora.com>

steam-runtime-tools/cpu-feature-internal.h
@@ -47,7 +47,24 @@
                               | SRT_X86_FEATURE_SSE3 \
                               | SRT_X86_FEATURE_CMPXCHG16B)
 
+typedef struct
+{
+  guint leaf;
+  guint subleaf;
+} SrtCpuidKey;
+
 #if defined(__x86_64__) || defined(__i386__)
+static inline SrtCpuidKey *
+_srt_cpuid_key_new (guint leaf,
+                    guint subleaf)
+{
+  SrtCpuidKey *ret = g_new0 (SrtCpuidKey, 1);
+
+  ret->leaf = leaf;
+  ret->subleaf = subleaf;
+  return ret;
+}
+
 typedef union
 {
   char text[17];
@@ -80,10 +97,22 @@ _srt_cpuid_data_new_for_signature (const char *text)
 #endif
 
 static inline void
+_srt_cpuid_key_free (gpointer self)
+{
+  g_free (self);
+}
+
+static inline void
 _srt_cpuid_data_free (gpointer self)
 {
   g_free (self);
 }
+
+G_GNUC_INTERNAL
+gboolean _srt_cpuid_key_equals (gconstpointer p1,
+                                gconstpointer p2);
+G_GNUC_INTERNAL
+guint _srt_cpuid_key_hash (gconstpointer key);
 
 #if defined(__x86_64__) || defined(__i386__)
 G_GNUC_INTERNAL

steam-runtime-tools/cpu-feature.c
@@ -49,7 +49,7 @@
 #if defined(__x86_64__) || defined(__i386__)
 /*
  * _srt_x86_cpuid:
- * @mock_cpuid: (nullable) (element-type guint SrtCpuidData): mock data
+ * @mock_cpuid: (nullable) (element-type SrtCpuidKey SrtCpuidData): mock data
  *  to use instead of the real CPUID instruction
  * @force: if %TRUE, use low-level `__cpuid()`, a thin wrapper around the
  *  CPUID instruction. If %FALSE, use `__get_cpuid()`, which does
@@ -75,8 +75,8 @@ gboolean _srt_x86_cpuid (GHashTable *mock_cpuid,
 {
   if (G_UNLIKELY (mock_cpuid != NULL))
     {
-      SrtCpuidData *mock = g_hash_table_lookup (mock_cpuid,
-                                                GUINT_TO_POINTER (leaf));
+      const SrtCpuidKey key = { leaf, 0 };
+      SrtCpuidData *mock = g_hash_table_lookup (mock_cpuid, &key);
 
       if (mock == NULL)
         return FALSE;
@@ -107,7 +107,7 @@ gboolean _srt_x86_cpuid (GHashTable *mock_cpuid,
 
 /*
  * _srt_x86_cpuid_count:
- * @mock_cpuid: (nullable) (element-type guint SrtCpuidData): mock data
+ * @mock_cpuid: (nullable) (element-type SrtCpuidKey SrtCpuidData): mock data
  *  to use instead of the real CPUID instruction
  * @leaf: CPUID leaf to return
  * @subleaf: CPUID subleaf to return
@@ -130,8 +130,8 @@ gboolean _srt_x86_cpuid_count (GHashTable *mock_cpuid,
 {
   if (G_UNLIKELY (mock_cpuid != NULL))
     {
-      SrtCpuidData *mock = g_hash_table_lookup (mock_cpuid,
-                                                GUINT_TO_POINTER ((guint64)subleaf << 32 | leaf));
+      const SrtCpuidKey key = { leaf, subleaf };
+      SrtCpuidData *mock = g_hash_table_lookup (mock_cpuid, &key);
 
       if (mock == NULL)
         return FALSE;
@@ -157,7 +157,7 @@ gboolean _srt_x86_cpuid_count (GHashTable *mock_cpuid,
 
 /**
  * _srt_feature_get_x86_flags:
- * @mock_cpuid: (nullable) (element-type guint SrtCpuidData): mock data
+ * @mock_cpuid: (nullable) (element-type SrtCpuidKey SrtCpuidData): mock data
  *  to use instead of the real CPUID instruction
  * @known: (out): Used to return the #SrtX86FeatureFlags
  *  that have been checked
@@ -213,4 +213,22 @@ _srt_feature_get_x86_flags (GHashTable *mock_cpuid,
 #endif
 
   return present;
+}
+
+gboolean
+_srt_cpuid_key_equals (gconstpointer p1,
+                       gconstpointer p2)
+{
+  const SrtCpuidKey *k1 = p1;
+  const SrtCpuidKey *k2 = p2;
+
+  return k1->leaf == k2->leaf && k1->subleaf == k2->subleaf;
+}
+
+guint
+_srt_cpuid_key_hash (gconstpointer key)
+{
+  const SrtCpuidKey *k = key;
+
+  return k->leaf ^ k->subleaf;
 }

steam-runtime-tools/virtualization.c
@@ -203,7 +203,7 @@ static const struct
 
 /**
  * _srt_check_virtualization:
- * @mock_cpuid: (nullable) (element-type guint SrtCpuidData): mock data
+ * @mock_cpuid: (nullable) (element-type SrtCpuidKey SrtCpuidData): mock data
  *  to use instead of the real CPUID instruction
  * @sysroot_fd: Sysroot file descriptor
  *

tests/cpu-feature.c
@@ -67,7 +67,9 @@ test_cpu_feature (Fixture *f,
 #if defined(__x86_64__) || defined(__i386__)
   SrtX86FeatureFlags known;
   SrtX86FeatureFlags present;
-  g_autoptr(GHashTable) mock_cpuid = g_hash_table_new_full (NULL, NULL, NULL,
+  g_autoptr(GHashTable) mock_cpuid = g_hash_table_new_full (_srt_cpuid_key_hash,
+                                                            _srt_cpuid_key_equals,
+                                                            _srt_cpuid_key_free,
                                                             _srt_cpuid_data_free);
 
   present = _srt_feature_get_x86_flags (NULL, &known);
@@ -85,44 +87,54 @@ test_cpu_feature (Fixture *f,
   g_assert_cmpint (known, ==, SRT_X86_FEATURE_NONE);
   g_assert_cmpint (present, ==, SRT_X86_FEATURE_NONE);
 
-  g_hash_table_replace (mock_cpuid, GUINT_TO_POINTER (_SRT_CPUID_LEAF_PROCESSOR_INFO),
+  g_hash_table_replace (mock_cpuid,
+                        _srt_cpuid_key_new (_SRT_CPUID_LEAF_PROCESSOR_INFO, 0),
                         _srt_cpuid_data_new (0, 0, 0, 0));
-  g_hash_table_replace (mock_cpuid, GUINT_TO_POINTER (_SRT_CPUID_LEAF_EXT_PROCESSOR_INFO),
+  g_hash_table_replace (mock_cpuid,
+                        _srt_cpuid_key_new (_SRT_CPUID_LEAF_EXT_PROCESSOR_INFO, 0),
                         _srt_cpuid_data_new (0, 0, 0, 0));
   present = _srt_feature_get_x86_flags (mock_cpuid, &known);
   g_assert_cmpint (known, ==, _SRT_X86_FEATURE_ALL);
   g_assert_cmpint (present, ==, SRT_X86_FEATURE_NONE);
 
-  g_hash_table_replace (mock_cpuid, GUINT_TO_POINTER (_SRT_CPUID_LEAF_PROCESSOR_INFO),
+  g_hash_table_replace (mock_cpuid,
+                        _srt_cpuid_key_new (_SRT_CPUID_LEAF_PROCESSOR_INFO, 0),
                         _srt_cpuid_data_new (0,
                                              0,
                                              (bit_CMPXCHG16B | bit_SSE3),
                                              0));
-  g_hash_table_replace (mock_cpuid, GUINT_TO_POINTER (_SRT_CPUID_LEAF_EXT_PROCESSOR_INFO),
+  g_hash_table_replace (mock_cpuid,
+                        _srt_cpuid_key_new (_SRT_CPUID_LEAF_EXT_PROCESSOR_INFO, 0),
                         _srt_cpuid_data_new (0, 0, 0, bit_LM));
   present = _srt_feature_get_x86_flags (mock_cpuid, &known);
   g_assert_cmpint (known, ==, _SRT_X86_FEATURE_ALL);
   g_assert_cmpint (present, ==, _SRT_X86_FEATURE_ALL);
 
-  g_hash_table_replace (mock_cpuid, GUINT_TO_POINTER (_SRT_CPUID_LEAF_PROCESSOR_INFO),
+  g_hash_table_replace (mock_cpuid,
+                        _srt_cpuid_key_new (_SRT_CPUID_LEAF_PROCESSOR_INFO, 0),
                         _srt_cpuid_data_new (0, 0, bit_CMPXCHG16B, 0));
-  g_hash_table_replace (mock_cpuid, GUINT_TO_POINTER (_SRT_CPUID_LEAF_EXT_PROCESSOR_INFO),
+  g_hash_table_replace (mock_cpuid,
+                        _srt_cpuid_key_new (_SRT_CPUID_LEAF_EXT_PROCESSOR_INFO, 0),
                         _srt_cpuid_data_new (0, 0, 0, 0));
   present = _srt_feature_get_x86_flags (mock_cpuid, &known);
   g_assert_cmpint (known, ==, _SRT_X86_FEATURE_ALL);
   g_assert_cmpint (present, ==, SRT_X86_FEATURE_CMPXCHG16B);
 
-  g_hash_table_replace (mock_cpuid, GUINT_TO_POINTER (_SRT_CPUID_LEAF_PROCESSOR_INFO),
+  g_hash_table_replace (mock_cpuid,
+                        _srt_cpuid_key_new (_SRT_CPUID_LEAF_PROCESSOR_INFO, 0),
                         _srt_cpuid_data_new (0, 0, bit_SSE3, 0));
-  g_hash_table_replace (mock_cpuid, GUINT_TO_POINTER (_SRT_CPUID_LEAF_EXT_PROCESSOR_INFO),
+  g_hash_table_replace (mock_cpuid,
+                        _srt_cpuid_key_new (_SRT_CPUID_LEAF_EXT_PROCESSOR_INFO, 0),
                         _srt_cpuid_data_new (0, 0, 0, 0));
   present = _srt_feature_get_x86_flags (mock_cpuid, &known);
   g_assert_cmpint (known, ==, _SRT_X86_FEATURE_ALL);
   g_assert_cmpint (present, ==, SRT_X86_FEATURE_SSE3);
 
-  g_hash_table_replace (mock_cpuid, GUINT_TO_POINTER (_SRT_CPUID_LEAF_PROCESSOR_INFO),
+  g_hash_table_replace (mock_cpuid,
+                        _srt_cpuid_key_new (_SRT_CPUID_LEAF_PROCESSOR_INFO, 0),
                         _srt_cpuid_data_new (0, 0, 0, 0));
-  g_hash_table_replace (mock_cpuid, GUINT_TO_POINTER (_SRT_CPUID_LEAF_EXT_PROCESSOR_INFO),
+  g_hash_table_replace (mock_cpuid,
+                        _srt_cpuid_key_new (_SRT_CPUID_LEAF_EXT_PROCESSOR_INFO, 0),
                         _srt_cpuid_data_new (0, 0, 0, bit_LM));
   present = _srt_feature_get_x86_flags (mock_cpuid, &known);
   g_assert_cmpint (known, ==, _SRT_X86_FEATURE_ALL);

tests/virtualization.c
@@ -87,7 +87,9 @@ test_cpuid (Fixture *f,
 {
   G_GNUC_UNUSED const Config *config = context;
   g_autoptr(GError) error = NULL;
-  g_autoptr(GHashTable) mock_cpuid = g_hash_table_new_full (NULL, NULL, NULL,
+  g_autoptr(GHashTable) mock_cpuid = g_hash_table_new_full (_srt_cpuid_key_hash,
+                                                            _srt_cpuid_key_equals,
+                                                            _srt_cpuid_key_free,
                                                             _srt_cpuid_data_free);
   /* We use the debian10 mock sysroot, which doesn't have a /sys,
    * to ensure that only CPUID gets used. Initially, there is no CPUID
@@ -115,7 +117,8 @@ test_cpuid (Fixture *f,
       g_autoptr(SrtVirtualizationInfo) virt = NULL;
 
       g_hash_table_remove_all (mock_cpuid);
-      g_hash_table_replace (mock_cpuid, GUINT_TO_POINTER (_SRT_CPUID_LEAF_PROCESSOR_INFO),
+      g_hash_table_replace (mock_cpuid,
+                            _srt_cpuid_key_new (_SRT_CPUID_LEAF_PROCESSOR_INFO, 0),
                             _srt_cpuid_data_new (0, 0, _SRT_CPUID_FLAG_PROCESSOR_INFO_ECX_HYPERVISOR_PRESENT, 0));
       virt = _srt_check_virtualization (mock_cpuid, "", sysroot_fd);
       g_assert_nonnull (virt);
@@ -130,9 +133,11 @@ test_cpuid (Fixture *f,
       g_autoptr(SrtVirtualizationInfo) virt = NULL;
 
       g_hash_table_remove_all (mock_cpuid);
-      g_hash_table_replace (mock_cpuid, GUINT_TO_POINTER (_SRT_CPUID_LEAF_PROCESSOR_INFO),
+      g_hash_table_replace (mock_cpuid,
+                            _srt_cpuid_key_new (_SRT_CPUID_LEAF_PROCESSOR_INFO, 0),
                             _srt_cpuid_data_new (0, 0, _SRT_CPUID_FLAG_PROCESSOR_INFO_ECX_HYPERVISOR_PRESENT, 0));
-      g_hash_table_replace (mock_cpuid, GUINT_TO_POINTER (_SRT_CPUID_LEAF_HYPERVISOR_ID),
+      g_hash_table_replace (mock_cpuid,
+                            _srt_cpuid_key_new (_SRT_CPUID_LEAF_HYPERVISOR_ID, 0),
                             _srt_cpuid_data_new_for_signature ("xxxxKVMKVMKVM"));
       virt = _srt_check_virtualization (mock_cpuid, "", sysroot_fd);
       g_assert_nonnull (virt);
@@ -147,11 +152,14 @@ test_cpuid (Fixture *f,
       g_autoptr(SrtVirtualizationInfo) virt = NULL;
 
       g_hash_table_remove_all (mock_cpuid);
-      g_hash_table_replace (mock_cpuid, GUINT_TO_POINTER (_SRT_CPUID_LEAF_PROCESSOR_INFO),
+      g_hash_table_replace (mock_cpuid,
+                            _srt_cpuid_key_new (_SRT_CPUID_LEAF_PROCESSOR_INFO, 0),
                             _srt_cpuid_data_new (0, 0, 0, 0));
-      g_hash_table_replace (mock_cpuid, GUINT_TO_POINTER (_SRT_CPUID_LEAF_HYPERVISOR_ID),
+      g_hash_table_replace (mock_cpuid,
+                            _srt_cpuid_key_new (_SRT_CPUID_LEAF_HYPERVISOR_ID, 0),
                             _srt_cpuid_data_new_for_signature ("xxxxFEXIFEXIEMU"));
-      g_hash_table_replace (mock_cpuid, GUINT_TO_POINTER (_SRT_CPUID_LEAF_FEX_INFO),
+      g_hash_table_replace (mock_cpuid,
+                            _srt_cpuid_key_new (_SRT_CPUID_LEAF_FEX_INFO, 0),
                             _srt_cpuid_data_new (FEX_ARCH_AARCH64, 0, 0, 0));
       virt = _srt_check_virtualization (mock_cpuid, "#FEX-2203-64-g8ad14728",
                                         sysroot_fd);
@@ -173,7 +181,9 @@ test_dmi_id (Fixture *f,
   G_GNUC_UNUSED const Config *config = context;
   g_autoptr(GError) error = NULL;
   /* Empty CPUID data, so that we only use the DMI IDs */
-  g_autoptr(GHashTable) mock_cpuid = g_hash_table_new_full (NULL, NULL, NULL,
+  g_autoptr(GHashTable) mock_cpuid = g_hash_table_new_full (_srt_cpuid_key_hash,
+                                                            _srt_cpuid_key_equals,
+                                                            _srt_cpuid_key_free,
                                                             _srt_cpuid_data_free);
 
   /* The fedora sysroot is set up by tests/generate-sysroots.py to identify
@@ -217,9 +227,11 @@ test_dmi_id (Fixture *f,
 #if defined(__x86_64__) || defined(__i386__)
       /* KVM from CPUID is not overwritten by QEMU from DMI ID */
       g_hash_table_remove_all (mock_cpuid);
-      g_hash_table_replace (mock_cpuid, GUINT_TO_POINTER (_SRT_CPUID_LEAF_PROCESSOR_INFO),
+      g_hash_table_replace (mock_cpuid,
+                            _srt_cpuid_key_new (_SRT_CPUID_LEAF_PROCESSOR_INFO, 0),
                             _srt_cpuid_data_new (0, 0, _SRT_CPUID_FLAG_PROCESSOR_INFO_ECX_HYPERVISOR_PRESENT, 0));
-      g_hash_table_replace (mock_cpuid, GUINT_TO_POINTER (_SRT_CPUID_LEAF_HYPERVISOR_ID),
+      g_hash_table_replace (mock_cpuid,
+                            _srt_cpuid_key_new (_SRT_CPUID_LEAF_HYPERVISOR_ID, 0),
                             _srt_cpuid_data_new_for_signature ("xxxxKVMKVMKVM"));
       virt = _srt_check_virtualization (mock_cpuid, "", sysroot_fd);
       g_assert_nonnull (virt);
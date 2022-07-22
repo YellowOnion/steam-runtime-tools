YellowOnion/steam-runtime-tools

commit 5c3fb0900546bb054db7ec74cc5ded099b924d3b

pv-wrap: Bypass FEX-Emu overlay for most top-level directories
The FEX-Emu "rootfs" is conceptually just a /usr (plus parts of /etc and
/var), just like the Steam Runtime. However, the rootfs versions that
are currently distributed contain some files in /etc, /run and so on.

When we open files with opendir() or access() or similar, FEX-Emu will
transparently substitute files from the "rootfs" if they exist. However,
when we're building up a list of bind-mounts for a root filesystem,
this is counterproductive: bwrap will do the equivalent of
`mount --bind /oldroot/run/foo /newroot/run/foo`, which is not going
to work if /foo is really /home/me/.fex-emu/RootFS/Ubuntu_21_10/run/foo.

For several categories of file, and in particular /run and other API
filesystems, what we really want to do is to ignore the "rootfs" and
take the files and directories from the real root. Because FEX-Emu's
rewriting is path-based rather than identity-based, we can do this by
looking up the paths relative to /proc/self/root.

Signed-off-by: Simon McVittie <smcv@collabora.com>

pressure-vessel/wrap.c
@@ -1424,12 +1424,25 @@ main (int argc,
 
   if (flatpak_subsandbox == NULL)
     {
+      glnx_autofd int root_fd = -1;
+
       /* Start with an empty environment and populate it later */
       bwrap = flatpak_bwrap_new (flatpak_bwrap_empty_env);
       g_assert (bwrap_executable != NULL);
       flatpak_bwrap_add_arg (bwrap, bwrap_executable);
       bwrap_filesystem_arguments = flatpak_bwrap_new (flatpak_bwrap_empty_env);
       exports = flatpak_exports_new ();
+
+      /* FEX-Emu transparently rewrites most file I/O to check its "rootfs"
+       * first, and only use the real root if the corresponding file
+       * doesn't exist in the "rootfs". We actively don't want this, because
+       * the FlatpakExports is inspecting these paths in order to pass them
+       * to bwrap, which will use them to set up bind-mounts, which are not
+       * subject to FEX-Emu's rewriting; so bypass it here. */
+      if (!glnx_opendirat (AT_FDCWD, "/proc/self/root", TRUE, &root_fd, error))
+        goto out;
+
+      flatpak_exports_take_host_fd (exports, glnx_steal_fd (&root_fd));
     }
   else
     {

pressure-vessel/bwrap.c
@@ -374,7 +374,7 @@ pv_bwrap_add_api_filesystems (FlatpakBwrap *bwrap,
 
   if (g_strcmp0 (link, "/run/shm") == 0)
     {
-      if (g_file_test ("/run/shm", G_FILE_TEST_IS_DIR))
+      if (g_file_test ("/proc/self/root/run/shm", G_FILE_TEST_IS_DIR))
         flatpak_bwrap_add_args (bwrap,
                                 "--bind", "/run/shm", "/run/shm",
                                 NULL);

pressure-vessel/wrap-setup.c
@@ -412,7 +412,13 @@ export_root_dirs_like_filesystem_host (FlatpakExports *exports,
   g_return_val_if_fail ((unsigned) mode <= FLATPAK_FILESYSTEM_MODE_LAST, FALSE);
   g_return_val_if_fail (error == NULL || *error == NULL, FALSE);
 
-  dir = g_dir_open ("/", 0, error);
+  /* FEX-Emu transparently rewrites most file I/O to check its "rootfs"
+   * first, and only use the real root if the corresponding file
+   * doesn't exist in the "rootfs". We actively don't want this, because
+   * we're inspecting these paths here in order to pass them to bwrap,
+   * which will use them to set up bind-mounts, which are not subject to
+   * FEX-Emu's rewriting; so bypass it here. */
+  dir = g_dir_open ("/proc/self/root", 0, error);
 
   if (dir == NULL)
     return FALSE;
@@ -460,7 +466,8 @@ export_contents_of_run (FlatpakBwrap *bwrap,
   g_return_val_if_fail (!g_file_test ("/.flatpak-info", G_FILE_TEST_IS_REGULAR),
                         FALSE);
 
-  dir = g_dir_open ("/run", 0, error);
+  /* Avoid FEX-Emu's path rewriting: see export_root_dirs_like_filesystem_host() */
+  dir = g_dir_open ("/proc/self/root/run", 0, error);
 
   if (dir == NULL)
     return FALSE;
@@ -510,8 +517,10 @@ pv_wrap_use_host_os (FlatpakExports *exports,
   for (i = 0; i < G_N_ELEMENTS (export_os_mutable); i++)
     {
       const char *dir = export_os_mutable[i];
+      /* Avoid FEX-Emu's path rewriting: see export_contents_of_run() */
+      g_autofree char *really = g_strdup_printf ("/proc/self/root%s", dir);
 
-      if (g_file_test (dir, G_FILE_TEST_EXISTS))
+      if (g_file_test (really, G_FILE_TEST_EXISTS))
         flatpak_bwrap_add_args (bwrap, "--bind", dir, dir, NULL);
     }
 

commit 0f7599c47427d7f7013ed228f20c495454b2a9dc

pv-wrap: Don't use fd-relative I/O for Flatpak-derived exports code
When I added support for a mock root file descriptor in FlatpakExports,
it was as design-for-test rather than for production use, so it is not
as thorough as the rest of pressure-vessel about resolving symlinks in
the mock root: some code paths will escape from the mock root, while
others will detect the escape and raise an error. Because this is code
that we share with Flatpak, we can't make it behave like the rest of
pressure-vessel unless we either diverge from Flatpak (increasing
maintenance cost), or copy _srt_resolve_in_sysroot() into Flatpak
(difficult to justify when Flatpak has no need for the increased
complexity).

Unfortunately, this is causing regressions when launching Steam games
when an exported filesystem path, or an ancestor or parent of an
exported filesystem path, is an absolute symbolic link. This was not
detected during development because the majority of exported filesystem
paths are either inside the home directory, inside the Steam library,
or inside ~/.steam, which are usually "real directories" with all of
their ancestors up to the root also being "real directories", resulting
in their contents not needing to be exported explicitly.

The FlatpakExports code detects that the absolute symlinks encountered
in this situation are "escaping" from /proc/self/fd/N/, and behaves as
though the symlink did not exist, causing important Steam objects to
fail to appear inside the container. For example, if ~/.steam is a
symbolic link, then symlinks like ~/.steam/root will be missing, breaking
parts of the Steamworks API.

We don't actually need to alter FlatpakExports' behaviour even in the
FEX-Emu case, because after converting the FlatpakExports object into
a sequence of bwrap arguments, we remap those arguments through
adjust_exports(), which handles the possibility that a path that is
apparently in the root filesystem is really in the FEX-Emu sysroot.
We do that because we need it for pv_bwrap_share_sockets(), which
calls into Flatpak-derived code that does not support specifying an
alternative root, but it'll work equally well for non-socket objects
like ~/.steam.

This partially reverts commit 5c3fb09.

Fixes: 5c3fb09 "pv-wrap: Bypass FEX-Emu overlay for most top-level directories"

pressure-vessel/wrap.c
@@ -1447,25 +1447,12 @@ main (int argc,
 
   if (flatpak_subsandbox == NULL)
     {
-      glnx_autofd int root_fd = -1;
-
       /* Start with an empty environment and populate it later */
       bwrap = flatpak_bwrap_new (flatpak_bwrap_empty_env);
       g_assert (bwrap_executable != NULL);
       flatpak_bwrap_add_arg (bwrap, bwrap_executable);
       bwrap_filesystem_arguments = flatpak_bwrap_new (flatpak_bwrap_empty_env);
       exports = flatpak_exports_new ();
-
-      /* FEX-Emu transparently rewrites most file I/O to check its "rootfs"
-       * first, and only use the real root if the corresponding file
-       * doesn't exist in the "rootfs". We actively don't want this, because
-       * the FlatpakExports is inspecting these paths in order to pass them
-       * to bwrap, which will use them to set up bind-mounts, which are not
-       * subject to FEX-Emu's rewriting; so bypass it here. */
-      if (!glnx_opendirat (AT_FDCWD, "/proc/self/root", TRUE, &root_fd, error))
-        goto out;
-
-      flatpak_exports_take_host_fd (exports, glnx_steal_fd (&root_fd));
     }
   else
     {
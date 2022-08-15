be0b43711da0f6135499e2bb861c9db5a7a9f05b
Ludovico de Nittis <ludovico.denittis@collabora.com>
2022-08-15 17:54:23 +0200

    runtime: Factor out EGL external platforms enumeration
    Signed-off-by: Ludovico de Nittis <ludovico.denittis@collabora.com>

--- pressure-vessel/runtime.c
+++ pressure-vessel/runtime.c
@@ -6233,6 +6233,51 @@ collect_dri_drivers (PvRuntime *self,
  * Returns: (element-type IcdDetails):
  */
 static GPtrArray *
+pv_enumerate_egl_ext_platforms (SrtSystemInfo *system_info,
+                                const char * const *multiarch_tuples)
+{
+  G_GNUC_UNUSED g_autoptr(SrtProfilingTimer) timer = NULL;
+  g_autoptr(SrtObjectList) egl_ext_platforms = NULL;
+  g_autoptr(GPtrArray) egl_ext_platform_details = NULL;
+  gsize i;
+  const GList *icd_iter;
+
+  timer = _srt_profiling_start ("Enumerating EGL external platforms");
+  g_debug ("Enumerating EGL external platforms on provider system...");
+  egl_ext_platforms = srt_system_info_list_egl_external_platforms (system_info,
+                                                                   multiarch_tuples);
+  egl_ext_platform_details = g_ptr_array_new_full (g_list_length (egl_ext_platforms),
+                                                   (GDestroyNotify) G_CALLBACK (icd_details_free));
+
+  for (icd_iter = egl_ext_platforms, i = 0;
+       icd_iter != NULL;
+       icd_iter = icd_iter->next, i++)
+    {
+      SrtEglExternalPlatform *ext = icd_iter->data;
+      const gchar *path = srt_egl_external_platform_get_json_path (ext);
+      GError *local_error = NULL;
+
+      if (!srt_egl_external_platform_check_error (ext, &local_error))
+        {
+          g_info ("Failed to load EGL external platform #%" G_GSIZE_FORMAT  " from %s: %s",
+                  i, path, local_error->message);
+          g_clear_error (&local_error);
+          continue;
+        }
+
+      g_info ("EGL external platform #%" G_GSIZE_FORMAT " at %s: %s",
+              i, path, srt_egl_external_platform_get_library_path (ext));
+
+      g_ptr_array_add (egl_ext_platform_details, icd_details_new (ext));
+    }
+
+  return g_steal_pointer (&egl_ext_platform_details);
+}
+
+/*
+ * Returns: (element-type IcdDetails):
+ */
+static GPtrArray *
 pv_enumerate_egl_icds (SrtSystemInfo *system_info,
                        const char * const *multiarch_tuples)
 {
@@ -6406,7 +6451,6 @@ pv_runtime_use_provider_graphics_stack (PvRuntime *self,
   g_autoptr(GString) va_api_path = g_string_new ("");
   gboolean any_architecture_works = FALSE;
   g_autoptr(SrtSystemInfo) system_info = NULL;
-  g_autoptr(SrtObjectList) egl_ext_platforms = NULL;
   g_autoptr(GPtrArray) egl_icd_details = NULL;      /* (element-type IcdDetails) */
   g_autoptr(GPtrArray) egl_ext_platform_details = NULL;   /* (element-type IcdDetails) */
   g_autoptr(GPtrArray) vulkan_icd_details = NULL;   /* (element-type IcdDetails) */
@@ -6414,8 +6458,6 @@ pv_runtime_use_provider_graphics_stack (PvRuntime *self,
   g_autoptr(GPtrArray) vulkan_imp_layer_details = NULL;   /* (element-type IcdDetails) */
   G_GNUC_UNUSED g_autoptr(SrtProfilingTimer) timer = NULL;
   g_autoptr(SrtProfilingTimer) part_timer = NULL;
-  guint n_egl_ext_platforms;
-  const GList *icd_iter;
   gboolean all_libglx_from_provider = TRUE;
   gboolean all_libdrm_from_provider = TRUE;
   g_autoptr(GHashTable) drirc_data_in_provider = g_hash_table_new_full (g_str_hash,
@@ -6450,38 +6492,7 @@ pv_runtime_use_provider_graphics_stack (PvRuntime *self,
 
   egl_icd_details = pv_enumerate_egl_icds (system_info, pv_multiarch_tuples);
 
-  part_timer = _srt_profiling_start ("Enumerating EGL external platforms");
-  g_debug ("Enumerating EGL external platforms on provider system...");
-  egl_ext_platforms = srt_system_info_list_egl_external_platforms (system_info,
-                                                                   pv_multiarch_tuples);
-  n_egl_ext_platforms = g_list_length (egl_ext_platforms);
-
-  egl_ext_platform_details = g_ptr_array_new_full (n_egl_ext_platforms,
-                                                   (GDestroyNotify) G_CALLBACK (icd_details_free));
-
-  for (icd_iter = egl_ext_platforms, j = 0;
-       icd_iter != NULL;
-       icd_iter = icd_iter->next, j++)
-    {
-      SrtEglExternalPlatform *ext = icd_iter->data;
-      const gchar *path = srt_egl_external_platform_get_json_path (ext);
-      GError *local_error = NULL;
-
-      if (!srt_egl_external_platform_check_error (ext, &local_error))
-        {
-          g_info ("Failed to load EGL external platform #%" G_GSIZE_FORMAT  " from %s: %s",
-                  j, path, local_error->message);
-          g_clear_error (&local_error);
-          continue;
-        }
-
-      g_info ("EGL external platform #%" G_GSIZE_FORMAT " at %s: %s",
-              j, path, srt_egl_external_platform_get_library_path (ext));
-
-      g_ptr_array_add (egl_ext_platform_details, icd_details_new (ext));
-    }
-
-  g_clear_pointer (&part_timer, _srt_profiling_end);
+  egl_ext_platform_details = pv_enumerate_egl_ext_platforms (system_info, pv_multiarch_tuples);
 
   vulkan_icd_details = pv_enumerate_vulkan_icds (system_info, pv_multiarch_tuples);
 

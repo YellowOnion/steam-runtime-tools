YellowOnion/steam-runtime-tools

commit 516b106c520739417e81e2d0707fc4c1a0b43329

log: Add Journal integration to GLib log handler
This has three main functions:

- If stdout and/or stderr are /dev/null, send warnings to the systemd
  Journal so they don't get lost, along with debug messages if enabled.

- If the user requests it, redirect stdout and/or stderr to the systemd
  Journal so that everything will be logged in one place.

- If stdout and/or stderr is already a Journal file descriptor
  inherited from a parent process, reopen it with our own details, and
  send our log messages with a bit more metadata.

This obsoletes _srt_util_set_up_logging(), which did the first two
of those.

This is a per-application opt-in, because each application that will make
use of this needs to be aware that it might redirect stdout and stderr.

Signed-off-by: Simon McVittie <smcv@collabora.com>

steam-runtime-tools/log-internal.h
@@ -39,6 +39,8 @@ typedef enum
   SRT_LOG_FLAGS_PID = (1 << 4),
   SRT_LOG_FLAGS_TIMING = (1 << 5),
   SRT_LOG_FLAGS_DIVERT_STDOUT = (1 << 6),
+  SRT_LOG_FLAGS_OPTIONALLY_JOURNAL = (1 << 7),
+  SRT_LOG_FLAGS_JOURNAL = (1 << 8),
   SRT_LOG_FLAGS_NONE = 0
 } SrtLogFlags;
 

bin/steam-remote.c
@@ -43,7 +43,10 @@ main (int argc,
   static const char *default_argv[] = {"steam", "-foreground", NULL};
   g_autoptr(GError) error = NULL;
 
-  _srt_util_set_up_logging ("steam-runtime-steam-remote");
+  _srt_util_set_glib_log_handler ("steam-runtime-steam-remote",
+                                  G_LOG_DOMAIN,
+                                  SRT_LOG_FLAGS_OPTIONALLY_JOURNAL,
+                                  NULL, NULL, NULL);
 
   if (argc <= 1)
     {

bin/urlopen.c
@@ -153,7 +153,10 @@ main (int argc,
   g_autoptr(GError) error = NULL;
   gboolean prefer_steam;
 
-  _srt_util_set_up_logging ("steam-runtime-urlopen");
+  _srt_util_set_glib_log_handler ("steam-runtime-urlopen",
+                                  G_LOG_DOMAIN,
+                                  SRT_LOG_FLAGS_OPTIONALLY_JOURNAL,
+                                  NULL, NULL, NULL);
 
   option_context = g_option_context_new ("{ file | URL }");
   g_option_context_add_main_entries (option_context, option_entries, NULL);

steam-runtime-tools/log.c
@@ -87,6 +87,8 @@ G_DEFINE_AUTO_CLEANUP_FREE_FUNC (AutoLibraryHandle, dlclose, NULL);
 
 typedef int (*type_of_sd_journal_stream_fd) (const char *, int, int);
 static type_of_sd_journal_stream_fd our_sd_journal_stream_fd;
+typedef int (*type_of_sd_journal_send) (const char *, ...);
+static type_of_sd_journal_send our_sd_journal_send = NULL;
 
 static int
 no_sd_journal_stream_fd (const char *identifier,
@@ -95,6 +97,9 @@ no_sd_journal_stream_fd (const char *identifier,
 {
   return -ENOSYS;
 }
+
+#define LOAD_OPTIONAL_SYMBOL(handle, name) \
+  our_ ## name = (type_of_ ## name) dlsym (handle, #name);
 
 /*
  * _srt_stdio_to_journal:
@@ -130,6 +135,8 @@ _srt_stdio_to_journal (const char *identifier,
           g_once_init_leave (&our_sd_journal_stream_fd, no_sd_journal_stream_fd);
           return glnx_throw (error, "%s", dlerror ());
         }
+
+      LOAD_OPTIONAL_SYMBOL (handle, sd_journal_send);
 
       (void) dlerror ();
       sym = (type_of_sd_journal_stream_fd) dlsym (handle, "sd_journal_stream_fd");
@@ -183,76 +190,13 @@ st_buf_is_dev_null (const struct stat *stat_buf)
   return (stat_buf->st_rdev == ((1 << 8) + 3));
 }
 
-/*
- * _srt_util_set_up_logging:
- * @identifier: the name of this program, passed to
- *  g_set_prgname() and `sd_journal_stream_fd`
- *
- * Set up logging for a command-line program such as steam-runtime-urlopen.
- */
-void
-_srt_util_set_up_logging (const char *identifier)
-{
-  struct stat stat_buf = {};
-
-  g_return_if_fail (identifier != NULL);
-
-  g_set_prgname (identifier);
-
-  /* If specifically told to use the Journal, do so. */
-  if (_srt_boolean_environment ("SRT_LOG_TO_JOURNAL", FALSE))
-    {
-      g_autoptr(GError) local_error = NULL;
-
-      if (!_srt_stdio_to_journal (identifier, STDOUT_FILENO, LOG_INFO, &local_error))
-        {
-          g_warning ("%s: %s", identifier, local_error->message);
-          g_clear_error (&local_error);
-        }
-
-      if (!_srt_stdio_to_journal (identifier, STDERR_FILENO, LOG_NOTICE, &local_error))
-        {
-          g_warning ("%s: %s", identifier, local_error->message);
-          g_clear_error (&local_error);
-        }
-
-      return;
-    }
-
-  /* If stdout is /dev/null, replace it with the Journal. */
-  if (fstat (STDOUT_FILENO, &stat_buf) != 0)
-    {
-      g_warning ("%s: Unable to stat stdout: %s",
-                 identifier, g_strerror (errno));
-    }
-  else if (st_buf_is_dev_null (&stat_buf))
-    {
-      g_autoptr(GError) local_error = NULL;
-
-      if (!_srt_stdio_to_journal (identifier, STDOUT_FILENO, LOG_INFO, &local_error))
-        g_warning ("%s: %s", identifier, local_error->message);
-    }
-
-  /* If stderr is /dev/null, replace it with the Journal. */
-  if (fstat (STDERR_FILENO, &stat_buf) != 0)
-    {
-      g_warning ("%s: Unable to stat stderr: %s",
-                 identifier, g_strerror (errno));
-    }
-  else if (st_buf_is_dev_null (&stat_buf))
-    {
-      g_autoptr(GError) local_error = NULL;
-
-      if (!_srt_stdio_to_journal (identifier, STDERR_FILENO, LOG_NOTICE, &local_error))
-        g_warning ("%s: %s", identifier, local_error->message);
-    }
-}
-
 static struct
 {
   int pid;
   const gchar *prgname;
   SrtLogFlags flags;
+  /* Non-NULL if and only if stderr was set up to be the Journal */
+  type_of_sd_journal_send journal_send;
 } log_settings =
 {
   .pid = -1,
@@ -293,6 +237,37 @@ get_level_prefix (GLogLevelFlags log_level)
 }
 
 /*
+ * get_level_priority:
+ * @log_level: A GLib log level, which should normally only have one bit set
+ *
+ * Returns: A syslog priority
+ */
+static int
+get_level_priority (GLogLevelFlags log_level)
+{
+  if (log_level & (G_LOG_FLAG_RECURSION
+                   | G_LOG_FLAG_FATAL
+                   | G_LOG_LEVEL_ERROR
+                   | SRT_LOG_LEVEL_FAILURE
+                   | G_LOG_LEVEL_CRITICAL))
+    return LOG_ERR;
+
+  if (log_level & (SRT_LOG_LEVEL_WARNING | G_LOG_LEVEL_WARNING))
+    return LOG_WARNING;
+
+  if (log_level & G_LOG_LEVEL_MESSAGE)
+    return LOG_NOTICE;
+
+  if (log_level & G_LOG_LEVEL_INFO)
+    return LOG_INFO;
+
+  if (log_level & G_LOG_LEVEL_DEBUG)
+    return LOG_DEBUG;
+
+  return LOG_NOTICE;
+}
+
+/*
  * log_handler:
  * @log_domain: the log domain of the message
  * @log_level: the log level of the message
@@ -320,10 +295,24 @@ log_handler (const gchar *log_domain,
                                           g_date_time_get_microsecond (date_time));
     }
 
-  g_printerr ("%s%s[%d]: %s: %s\n",
-              (timestamp_prefix ?: ""),
-              log_settings.prgname, log_settings.pid,
-              get_level_prefix (log_level), message);
+  /* We only set this to be non-NULL if connecting to the Journal succeeded */
+  if (log_settings.journal_send != NULL)
+    {
+      log_settings.journal_send ("GLIB_DOMAIN=%s", log_domain,
+                                 "MESSAGE=%s: %s",
+                                 get_level_prefix (log_level), message,
+                                 "PRIORITY=%d", get_level_priority (log_level),
+                                 "SYSLOG_IDENTIFIER=%s", log_settings.prgname,
+                                 NULL);
+    }
+
+  if (log_settings.journal_send == NULL)
+    {
+      g_printerr ("%s%s[%d]: %s: %s\n",
+                  (timestamp_prefix ?: ""),
+                  log_settings.prgname, log_settings.pid,
+                  get_level_prefix (log_level), message);
+    }
 
   if (log_level & (G_LOG_FLAG_RECURSION
                    | G_LOG_FLAG_FATAL
@@ -349,6 +338,12 @@ log_handler (const gchar *log_domain,
  *  being written to the original standard output
  *  (use this in conjunction with the @original_stdout_out parameter of
  *  _srt_util_set_glib_log_handler())
+ * @SRT_LOG_FLAGS_OPTIONALLY_JOURNAL: If standard output or standard error
+ *  is /dev/null or an invalid file descriptor, or if the user requests
+ *  logging to the Journal via environment variables, automatically
+ *  enable %SRT_LOG_FLAGS_JOURNAL.
+ * @SRT_LOG_FLAGS_JOURNAL: Try to write log messages to the systemd
+ *  Journal, and redirect standard output and standard error there.
  * @SRT_LOG_FLAGS_NONE: None of the above
  *
  * Flags affecting logging.
@@ -362,9 +357,17 @@ static const GDebugKey log_enable[] =
   { "diffable", SRT_LOG_FLAGS_DIFFABLE },
   { "pid", SRT_LOG_FLAGS_PID },
   { "timing", SRT_LOG_FLAGS_TIMING },
-  /* Intentionally no way to set SRT_LOG_FLAGS_DIVERT_STDOUT in $SRT_LOG:
-   * implementing that flag correctly requires the application to be aware
-   * that the original stdout might get altered */
+
+  /* Intentionally no way to set
+   * - SRT_LOG_FLAGS_DIVERT_STDOUT
+   * - SRT_LOG_FLAGS_OPTIONALLY_JOURNAL
+   * via $SRT_LOG: implementing those flags correctly requires the application
+   * to be aware that the original stdout might get altered */
+
+  /* Order matters: _srt_util_set_glib_log_handler() relies on this being
+   * the last one, so that it can be disabled in the absence of
+   * SRT_LOG_FLAGS_OPTIONALLY_JOURNAL */
+  { "journal", SRT_LOG_FLAGS_JOURNAL },
 };
 
 /*
@@ -442,6 +445,9 @@ set_up_output (int fd,
                int *save_original,
                GError **error)
 {
+  SrtLogFlags required_flags;
+  gboolean use_journal = FALSE;
+
   if (save_original != NULL)
     {
       *save_original = TEMP_FAILURE_RETRY (fcntl (fd, F_DUPFD_CLOEXEC, 3));
@@ -455,6 +461,82 @@ set_up_output (int fd,
 
   if (!ensure_fd_not_cloexec (fd, error))
     return FALSE;
+
+  if (fd == STDOUT_FILENO)
+    required_flags = SRT_LOG_FLAGS_JOURNAL | SRT_LOG_FLAGS_DIVERT_STDOUT;
+  else
+    required_flags = SRT_LOG_FLAGS_JOURNAL;
+
+  if ((log_settings.flags & required_flags) == required_flags)
+    use_journal = TRUE;
+
+  /* If it's already pointing to the Journal, open a new Journal stream so
+   * that a parent process doesn't get "blamed" for messages that we emit. */
+  if (g_log_writer_is_journald (fd))
+    use_journal = TRUE;
+
+  /* If it's /dev/null, replace it with the Journal if requested.
+   * No need to do this check if we're going to replace it with the
+   * Journal anyway. */
+  if ((log_settings.flags & SRT_LOG_FLAGS_OPTIONALLY_JOURNAL) && !use_journal)
+    {
+      struct stat stat_buf = {};
+
+      if (fstat (fd, &stat_buf) != 0)
+        {
+          _srt_log_warning ("Unable to stat fd %d: %s",
+                            fd, g_strerror (errno));
+        }
+      else if (st_buf_is_dev_null (&stat_buf))
+        {
+          use_journal = TRUE;
+        }
+    }
+
+  if (use_journal)
+    {
+      g_autoptr(GError) local_error = NULL;
+      int priority;
+
+      if (fd == STDERR_FILENO)
+        {
+          g_info ("Redirecting logging and stderr to systemd journal");
+          priority = LOG_NOTICE;
+        }
+      else
+        {
+          g_info ("Redirecting stdout to systemd journal");
+          priority = LOG_INFO;
+        }
+
+      /* Unstructured text on stdout/stderr becomes unstructured messages */
+      if (_srt_stdio_to_journal (log_settings.prgname, fd, priority, &local_error))
+        {
+          /* No need to redirect stdout to stderr if stdout is already a
+           * separate Journal stream */
+          if (fd == STDOUT_FILENO)
+            log_settings.flags &= ~SRT_LOG_FLAGS_DIVERT_STDOUT;
+
+          /* Structured GLib logging on stderr becomes structured messages */
+          if (fd == STDERR_FILENO)
+            {
+              log_settings.flags |= SRT_LOG_FLAGS_JOURNAL;
+              log_settings.journal_send = our_sd_journal_send;
+            }
+        }
+      else
+        {
+          if (fd == STDERR_FILENO)
+            {
+              log_settings.flags &= ~SRT_LOG_FLAGS_JOURNAL;
+            }
+
+          /* Just emit a warning instead of failing: this can legitimately
+           * fail on systems that don't use the systemd Journal. */
+          _srt_log_warning ("%s", local_error->message);
+          g_clear_error (&local_error);
+        }
+    }
 
   return TRUE;
 }
@@ -526,11 +608,30 @@ _srt_util_set_glib_log_handler (const char *prgname,
                                | G_LOG_LEVEL_WARNING
                                | G_LOG_LEVEL_MESSAGE);
   const char *log_env = g_getenv ("SRT_LOG");
+  gsize log_env_n_keys = G_N_ELEMENTS (log_enable);
 
   if (prgname != NULL)
     g_set_prgname (prgname);
 
-  flags |= g_parse_debug_string (log_env, log_enable, G_N_ELEMENTS (log_enable));
+  if (flags & SRT_LOG_FLAGS_OPTIONALLY_JOURNAL)
+    {
+      /* Some CLI tools accepted this as an alternative to SRT_LOG=journal,
+       * so check both */
+      if (_srt_boolean_environment ("SRT_LOG_TO_JOURNAL", FALSE))
+        flags |= SRT_LOG_FLAGS_JOURNAL;
+    }
+  else
+    {
+      /* Don't allow SRT_LOG=journal to take effect if the application
+       * was not expecting it */
+      log_env_n_keys--;
+    }
+
+  flags |= g_parse_debug_string (log_env, log_enable, log_env_n_keys);
+
+  /* Specifically setting SRT_LOG_TO_JOURNAL=0 does the opposite */
+  if (!_srt_boolean_environment ("SRT_LOG_TO_JOURNAL", TRUE))
+    flags &= ~SRT_LOG_FLAGS_JOURNAL;
 
   if (_srt_boolean_environment ("PRESSURE_VESSEL_LOG_WITH_TIMESTAMP", FALSE))
     flags |= SRT_LOG_FLAGS_TIMESTAMP;
@@ -568,7 +669,7 @@ _srt_util_set_glib_log_handler (const char *prgname,
       || !set_up_output (STDERR_FILENO, original_stderr_out, error))
     return FALSE;
 
-  if (flags & SRT_LOG_FLAGS_DIVERT_STDOUT)
+  if (log_settings.flags & SRT_LOG_FLAGS_DIVERT_STDOUT)
     {
       /* Unusually, intentionally not setting FD_CLOEXEC here */
       if (dup2 (STDERR_FILENO, STDOUT_FILENO) != STDOUT_FILENO)

commit 804934e1861d812e71e521846573054224d345c7

Remove unimplemented declaration
Fixes: 516b106c "log: Add Journal integration to GLib log handler"
Signed-off-by: Simon McVittie <smcv@collabora.com>

steam-runtime-tools/log-internal.h
@@ -58,7 +58,6 @@ gboolean _srt_util_set_glib_log_handler (const char *prgname,
                                          int *original_stdout_out,
                                          int *original_stderr_out,
                                          GError **error);
-void _srt_util_set_up_logging (const char *identifier);
 gboolean _srt_util_restore_saved_fd (int saved_fd,
                                      int target_fd,
                                      GError **error);
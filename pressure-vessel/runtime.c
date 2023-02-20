YellowOnion/steam-runtime-tools

commit c2fd0c62e18d534a25a75d3cd8a7a2552c11febf

pressure-vessel: Reuse the drivers basename
When creating the JSON manifests, we were simply naming them
sequentially, as an easy way to avoid conflicts.

However, since Vulkan Loader 1.3.234, it is possible to select which
driver to use by listing their names in the environment variable
`VK_LOADER_DRIVERS_SELECT`. This functionality would not work in
pressure-vessel, unless we keep the original JSON manifest name.

With this commit now we always reuse the JSON manifest basename.
In the rare events where this could cause a conflict, we create
unique sub directories to avoid issues.

Signed-off-by: Ludovico de Nittis <ludovico.denittis@collabora.com>

pressure-vessel/runtime.c
@@ -4607,6 +4607,10 @@ pv_runtime_take_ld_so_from_provider (PvRuntime *self,
  *  whichever is appropriate for @sub_dir
  * @digits: Number of digits to pad length of numeric prefix
  * @seq: Sequence number of @details, used to make unique filenames
+ * @json_set: (element-type filename ignored): A map
+ *  `{ owned string => itself }` representing the set
+ *  of JSON manifests already created. Used internally to notice when
+ *  to use unique sub directories to avoid naming conflicts
  * @search_path: Used to build `$VK_DRIVER_FILES` or a similar search path
  * @error: Used to raise an error on failure
  *
@@ -4619,6 +4623,7 @@ setup_json_manifest (PvRuntime *self,
                      IcdDetails *details,
                      int digits,
                      gsize seq,
+                     GHashTable *json_set,
                      GString *search_path,
                      GError **error)
 {
@@ -4628,38 +4633,48 @@ setup_json_manifest (PvRuntime *self,
   SrtEglExternalPlatform *ext_platform = NULL;
   gboolean loaded = FALSE;
   gboolean need_provider_json = FALSE;
+  g_autofree gchar *json_basename = NULL;
+  const char *json_in_provider = NULL;
   gsize i;
 
   g_return_val_if_fail (self->provider != NULL, FALSE);
   g_return_val_if_fail (bwrap != NULL || self->mutable_sysroot != NULL, FALSE);
-
-  g_debug ("Setting up JSON manifest for %s loadable module #%" G_GSIZE_FORMAT ": %s",
-           sub_dir, seq, details->debug_name);
+  g_return_val_if_fail (sub_dir != NULL, FALSE);
+  g_return_val_if_fail (json_set != NULL, FALSE);
 
   if (SRT_IS_VULKAN_LAYER (details->icd))
     {
       layer = SRT_VULKAN_LAYER (details->icd);
       loaded = srt_vulkan_layer_check_error (layer, NULL);
+      json_in_provider = srt_vulkan_layer_get_json_path (layer);
     }
   else if (SRT_IS_VULKAN_ICD (details->icd))
     {
       icd = SRT_VULKAN_ICD (details->icd);
       loaded = srt_vulkan_icd_check_error (icd, NULL);
+      json_in_provider = srt_vulkan_icd_get_json_path (icd);
     }
   else if (SRT_IS_EGL_ICD (details->icd))
     {
       egl = SRT_EGL_ICD (details->icd);
       loaded = srt_egl_icd_check_error (egl, NULL);
+      json_in_provider = srt_egl_icd_get_json_path (egl);
     }
   else if (SRT_IS_EGL_EXTERNAL_PLATFORM (details->icd))
     {
       ext_platform = SRT_EGL_EXTERNAL_PLATFORM (details->icd);
       loaded = srt_egl_external_platform_check_error (ext_platform, NULL);
+      json_in_provider = srt_egl_external_platform_get_json_path (ext_platform);
     }
   else
     {
       g_return_val_if_reached (FALSE);
     }
+
+  json_basename = g_path_get_basename (json_in_provider);
+
+  g_debug ("Setting up JSON manifest for \"%s\" loadable module \"%s\": \"%s\"",
+           sub_dir, json_basename, details->debug_name);
 
   /* If the layer failed to load, there's nothing to make available */
   if (!loaded)
@@ -4675,17 +4690,17 @@ setup_json_manifest (PvRuntime *self,
 
       if (details->kinds[i] == ICD_KIND_ABSOLUTE)
         {
-          g_autofree gchar *relative_to_overrides = NULL;
           g_autofree gchar *write_to_file = NULL;
           g_autofree gchar *write_to_dir = NULL;
           g_autofree gchar *json_in_container = NULL;
-          g_autofree gchar *json_base = NULL;
+          g_autofree gchar *relative_to_overrides = NULL;
 
           g_assert (details->paths_in_container[i] != NULL);
 
-          json_base = g_strdup_printf ("%.*" G_GSIZE_FORMAT "-%s.json",
-                                       digits, seq, pv_multiarch_tuples[i]);
-          relative_to_overrides = g_build_filename (sub_dir, json_base, NULL);
+          relative_to_overrides = pv_generate_unique_filepath (sub_dir, digits, seq,
+                                                               json_basename,
+                                                               pv_multiarch_tuples[i],
+                                                               json_set);
           write_to_file = g_build_filename (self->overrides,
                                             relative_to_overrides, NULL);
           write_to_dir = g_path_get_dirname (write_to_file);
@@ -4746,29 +4761,21 @@ setup_json_manifest (PvRuntime *self,
       else if (details->kinds[i] == ICD_KIND_SONAME
                || details->kinds[i] == ICD_KIND_META_LAYER)
         {
-          g_debug ("Will use graphics stack provider JSON as-is for %s #%" G_GSIZE_FORMAT,
-                   sub_dir, seq);
+          g_debug ("Will use graphics stack provider JSON as-is for %s/%s",
+                   sub_dir, json_basename);
           need_provider_json = TRUE;
         }
     }
 
   if (need_provider_json)
     {
+      g_autofree gchar *relative_to_overrides = NULL;
       g_autofree gchar *json_in_container = NULL;
-      g_autofree gchar *json_base = NULL;
-      const char *json_in_provider = NULL;
-      if (layer != NULL)
-        json_in_provider = srt_vulkan_layer_get_json_path (layer);
-      else if (egl != NULL)
-        json_in_provider = srt_egl_icd_get_json_path (egl);
-      else if (ext_platform != NULL)
-        json_in_provider = srt_egl_external_platform_get_json_path (ext_platform);
-      else
-        json_in_provider = srt_vulkan_icd_get_json_path (icd);
 
-      json_base = g_strdup_printf ("%.*" G_GSIZE_FORMAT ".json", digits, seq);
+      relative_to_overrides = pv_generate_unique_filepath (sub_dir, digits, seq, json_basename,
+                                                           NULL, json_set);
       json_in_container = g_build_filename (self->overrides_in_container,
-                                            sub_dir, json_base, NULL);
+                                            relative_to_overrides, NULL);
 
       g_debug ("Copying \"%s\" as-is to implement \"%s\" in container",
                json_in_provider, json_in_container);
@@ -4809,16 +4816,19 @@ setup_each_json_manifest (PvRuntime *self,
                           GError **error)
 {
   gsize j;
+  g_autoptr(GHashTable) json_set = NULL;
   int digits = pv_count_decimal_digits (details->len);
 
   g_return_val_if_fail (self->provider != NULL, FALSE);
   g_return_val_if_fail (bwrap != NULL || self->mutable_sysroot != NULL, FALSE);
 
+  json_set = g_hash_table_new_full (g_str_hash, g_str_equal, g_free, NULL);
+
   for (j = 0; j < details->len; j++)
     {
       if (!setup_json_manifest (self, bwrap, sub_dir,
                                 g_ptr_array_index (details, j),
-                                digits, j, search_path, error))
+                                digits, j, json_set, search_path, error))
         return FALSE;
     }
 

pressure-vessel/utils.c
@@ -807,3 +807,58 @@ pv_count_decimal_digits (gsize n)
 
   return required;
 }
+
+/*
+ * pv_generate_unique_filepath:
+ * @sub_dir: `share/vulkan/icd.d`, `share/glvnd/egl_vendor.d` or similar
+ * @digits: Number of digits to pad length of numeric prefix if used
+ * @seq: Sequence number of files, used to make unique filenames
+ * @file: basename of the file
+ * @multiarch_tuple: (nullable): Multiarch tuple of the @file, used
+ *  to make unique filenames, or %NULL if architecture-independent
+ * @files_set: (element-type filename ignored): A map
+ *  `{ owned string => itself }` representing the set
+ *  of files for which it has already been generated a unique path. Used
+ *  internally to notice when to use unique sub directories.
+ *
+ * Generate a file path for @file, under @sub_dir, and store it in
+ * @files_set. If "@sub_dir/@file" was already present in @files_set,
+ * a unique subdirectory based on @seq and @multiarch_tuple will be used.
+ *
+ * Returns: (transfer full) (type filename): The unique relative path for @file
+ *  in @sub_dir
+ */
+gchar *
+pv_generate_unique_filepath (const gchar *sub_dir,
+                             int digits,
+                             gsize seq,
+                             const gchar *file,
+                             const gchar *multiarch_tuple,
+                             GHashTable *files_set)
+{
+  g_autofree gchar *relative_to_overrides = NULL;
+
+  relative_to_overrides = g_build_filename (sub_dir, file, NULL);
+
+  /* If we already have a file with this name in this directory, we create
+   * a unique sub directory to avoid conflicts. */
+  if (g_hash_table_contains (files_set, relative_to_overrides))
+    {
+      g_autofree gchar *dedup_dir_basename = NULL;
+
+      g_clear_pointer (&relative_to_overrides, g_free);
+
+      if (multiarch_tuple != NULL)
+        dedup_dir_basename = g_strdup_printf ("%.*" G_GSIZE_FORMAT "-%s", digits,
+                                              seq, multiarch_tuple);
+      else
+        dedup_dir_basename = g_strdup_printf ("%.*" G_GSIZE_FORMAT, digits, seq);
+
+      relative_to_overrides = g_build_filename (sub_dir, dedup_dir_basename,
+                                                file, NULL);
+    }
+
+  g_hash_table_add (files_set, g_strdup (relative_to_overrides));
+
+  return g_steal_pointer (&relative_to_overrides);
+}

pressure-vessel/utils.h
@@ -56,3 +56,10 @@ void pv_delete_dangling_symlink (int dirfd,
                                  const char *name);
 
 int pv_count_decimal_digits (gsize n);
+
+gchar *pv_generate_unique_filepath (const gchar *sub_dir,
+                                    int digits,
+                                    gsize seq,
+                                    const gchar *file,
+                                    const gchar *multiarch_tuple,
+                                    GHashTable *files_set);

tests/pressure-vessel/utils.c
@@ -124,6 +124,150 @@ test_count_decimal_digits (Fixture *f,
     g_assert_cmpint (pv_count_decimal_digits (tests[i].n), ==, tests[i].digits);
 }
 
+typedef struct
+{
+  const gchar *sub_dir;
+  const gchar *file;
+  const gchar *multiarch_tuple;
+  int seq;
+  int digits; /* Defaults to 1 */
+  const gchar *expected_path;
+} FilepathData;
+
+typedef struct
+{
+  FilepathData filepath_data[10];
+} GenerateFilepathTest;
+
+static void
+test_generate_unique_filepath (Fixture *f,
+                               gconstpointer context)
+{
+#define ICD_SUBDIR "share/vulkan/icd.d"
+#define EXPLICIT_LAYER_SUBDIR "share/vulkan/explicit_layer.d"
+
+  static const GenerateFilepathTest tests[] =
+  {
+    {
+      .filepath_data =
+      {
+        {
+          .sub_dir = ICD_SUBDIR,
+          .file = "radeon_icd.json",
+          .seq = 0,
+        },
+        {
+          .sub_dir = ICD_SUBDIR,
+          .file = "lvp_icd.json",
+          .multiarch_tuple = "x86_64",
+          .seq = 1,
+        },
+        {
+          .sub_dir = ICD_SUBDIR,
+          .file = "radeon_icd.json",
+          .seq = 2,
+          .expected_path = "share/vulkan/icd.d/2/radeon_icd.json",
+        },
+        {
+          .sub_dir = ICD_SUBDIR,
+          .file = "lvp_icd.json",
+          .multiarch_tuple = "x86_64",
+          .seq = 3,
+          .expected_path = "share/vulkan/icd.d/3-x86_64/lvp_icd.json",
+        },
+        {
+          .sub_dir = ICD_SUBDIR,
+          .file = "lvp_icd.json",
+          .multiarch_tuple = "i686",
+          .seq = 3,
+          .expected_path = "share/vulkan/icd.d/3-i686/lvp_icd.json",
+        },
+      }
+    },
+
+    {
+      .filepath_data =
+      {
+        {
+          .sub_dir = ICD_SUBDIR,
+          .file = "radeon_icd.x86_64.json",
+          .seq = 0,
+        },
+        {
+          .sub_dir = ICD_SUBDIR,
+          .file = "lvp_icd.json",
+          .seq = 1,
+        },
+        {
+          .sub_dir = ICD_SUBDIR,
+          .file = "my_custom.json",
+          .seq = 2,
+        },
+        {
+          /* Use a different sub directory. There shouldn't be any conflicts,
+           * even if the filename is the same. */
+          .sub_dir = EXPLICIT_LAYER_SUBDIR,
+          .file = "my_custom.json",
+          .seq = 3,
+        },
+        {
+          .sub_dir = EXPLICIT_LAYER_SUBDIR,
+          .file = "my_custom.json",
+          .seq = 4,
+          .digits = 2,
+          .expected_path = "share/vulkan/explicit_layer.d/04/my_custom.json",
+        },
+        {
+          .sub_dir = EXPLICIT_LAYER_SUBDIR,
+          .file = "my_custom.json",
+          .multiarch_tuple = "x86_64",
+          .seq = 5,
+          .digits = 2,
+          .expected_path = "share/vulkan/explicit_layer.d/05-x86_64/my_custom.json",
+        },
+      }
+    },
+  };
+
+  gsize i;
+  gsize j;
+
+  for (i = 0; i < G_N_ELEMENTS (tests); i++)
+    {
+      g_autoptr(GHashTable) files_set = NULL;
+
+      files_set = g_hash_table_new_full (g_str_hash, g_str_equal, g_free, NULL);
+
+      for (j = 0; tests[i].filepath_data[j].file != NULL; j++)
+        {
+          g_autofree gchar *unique_path = NULL;
+          const FilepathData data = tests[i].filepath_data[j];
+
+          unique_path = pv_generate_unique_filepath (data.sub_dir,
+                                                     data.digits == 0 ? 1 : data.digits,
+                                                     data.seq,
+                                                     data.file,
+                                                     data.multiarch_tuple,
+                                                     files_set);
+
+          if (data.expected_path == NULL)
+            {
+              /* This is the case where there isn't a potential conflict */
+              g_autofree gchar *expected = g_build_filename (data.sub_dir, data.file, NULL);
+              g_assert_cmpstr (unique_path, ==, expected);
+            }
+          else
+            {
+              g_assert_cmpstr (unique_path, ==, data.expected_path);
+            }
+
+          g_assert_true (g_hash_table_contains (files_set, unique_path));
+          g_assert_cmpint (g_hash_table_size (files_set), ==, j + 1);
+        }
+
+    }
+}
+
 static void
 test_run_sync (Fixture *f,
                gconstpointer context)
@@ -512,6 +656,8 @@ main (int argc,
               setup, test_count_decimal_digits, teardown);
   g_test_add ("/first-key", Fixture, NULL,
               setup, test_first_key, teardown);
+  g_test_add ("/generate-unique-filepath", Fixture, NULL,
+              setup, test_generate_unique_filepath, teardown);
   g_test_add ("/run-sync", Fixture, NULL,
               setup, test_run_sync, teardown);
   g_test_add ("/delete-dangling-symlink", Fixture, NULL,

commit abd620bb4b14bb049f4b7a29a0025ba8158fbe27

pv-runtime: Ensure Vulkan layer manifests are all in the same directory
Because implicit layers don't respect VK_LAYER_PATH, we rely on adding
/overrides/share to $XDG_DATA_DIRS as our way to get our modified JSON
manifests to be picked up. However, this will only work if all of our
layers are described by manifests directly inside
/overrides/share/vulkan/*_layers.d/, and putting the manifests in a
subdirectory of that directory will not work.

steamrt/tasks#239

Fixes: c2fd0c62 "pressure-vessel: Reuse the drivers basename"
Resolves: https://github.com/ValveSoftware/steam-runtime/issues/564
Co-authored-by: Ludovico de Nittis <ludovico.denittis@collabora.com>
Signed-off-by: Simon McVittie <smcv@collabora.com>

pressure-vessel/runtime.c
@@ -4697,10 +4697,24 @@ setup_json_manifest (PvRuntime *self,
 
           g_assert (details->paths_in_container[i] != NULL);
 
-          relative_to_overrides = pv_generate_unique_filepath (sub_dir, digits, seq,
-                                                               json_basename,
-                                                               pv_multiarch_tuples[i],
-                                                               json_set);
+          /* For layers, we know that the filename doesn't matter - choice
+           * of layers is based on manifest["layer"]["name"] - but we have
+           * to make sure they're all unique and in the same directory,
+           * because there is no equivalent of VK_DRIVER_FILES or
+           * VK_LAYER_PATH for implicit layers, so the only thing we can
+           * do is to add our directory to XDG_DATA_DIRS. Because we have
+           * to do this for implicit layers anyway, for simplicity we do
+           * the same thing for explicit layers. */
+          if (SRT_IS_VULKAN_LAYER (details->icd))
+            relative_to_overrides = g_strdup_printf ("%s/%.*" G_GSIZE_FORMAT "-%s.json",
+                                                     sub_dir, digits, seq,
+                                                     pv_multiarch_tuples[i]);
+          else
+            relative_to_overrides = pv_generate_unique_filepath (sub_dir, digits, seq,
+                                                                 json_basename,
+                                                                 pv_multiarch_tuples[i],
+                                                                 json_set);
+
           write_to_file = g_build_filename (self->overrides,
                                             relative_to_overrides, NULL);
           write_to_dir = g_path_get_dirname (write_to_file);
@@ -4772,8 +4786,13 @@ setup_json_manifest (PvRuntime *self,
       g_autofree gchar *relative_to_overrides = NULL;
       g_autofree gchar *json_in_container = NULL;
 
-      relative_to_overrides = pv_generate_unique_filepath (sub_dir, digits, seq, json_basename,
-                                                           NULL, json_set);
+      if (SRT_IS_VULKAN_LAYER (details->icd))
+        relative_to_overrides = g_strdup_printf ("%s/%.*" G_GSIZE_FORMAT ".json",
+                                                 sub_dir, digits, seq);
+      else
+        relative_to_overrides = pv_generate_unique_filepath (sub_dir, digits, seq,
+                                                             json_basename,
+                                                             NULL, json_set);
       json_in_container = g_build_filename (self->overrides_in_container,
                                             relative_to_overrides, NULL);
 
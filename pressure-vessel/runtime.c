YellowOnion/steam-runtime-tools

commit 451544d80fcfa487a244039dce14097cd7dd09b4

Add support for FEX-Emu graphics thunking
When we are using an interpreter like FEX, we have a FEX's rootfs, that
is usually x86-64, and the real host system root, that is usually
AArch64.

When setting up the runtime we can also include the real host graphics
DRIs/layers to support thunking. Then, based on what the loader needs,
the correct architecture libraries will be loaded at runtime.

Signed-off-by: Ludovico de Nittis <ludovico.denittis@collabora.com>

pressure-vessel/runtime.c
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2020 Collabora Ltd.
+ * Copyright © 2020-2022 Collabora Ltd.
  *
  * SPDX-License-Identifier: LGPL-2.1-or-later
  *
@@ -95,8 +95,10 @@ struct _PvRuntime
   gchar *runtime_files_on_host;
   const gchar *adverb_in_container;
   PvGraphicsProvider *provider;
+  PvGraphicsProvider *interpreter_host_provider;
   const gchar *host_in_current_namespace;
   EnumerationThread indep_thread;
+  EnumerationThread host_thread;
   EnumerationThread *arch_threads;
 
   PvRuntimeFlags flags;
@@ -122,6 +124,7 @@ enum {
   PROP_0,
   PROP_BUBBLEWRAP,
   PROP_GRAPHICS_PROVIDER,
+  PROP_INTERPRETER_HOST_PROVIDER,
   PROP_SOURCE,
   PROP_ORIGINAL_ENVIRON,
   PROP_FLAGS,
@@ -479,6 +482,10 @@ pv_runtime_get_property (GObject *object,
         g_value_set_object (value, self->provider);
         break;
 
+      case PROP_INTERPRETER_HOST_PROVIDER:
+        g_value_set_object (value, self->interpreter_host_provider);
+        break;
+
       case PROP_ORIGINAL_ENVIRON:
         g_value_set_boxed (value, self->original_environ);
         break;
@@ -525,6 +532,12 @@ pv_runtime_set_property (GObject *object,
         /* Construct-only */
         g_return_if_fail (self->provider == NULL);
         self->provider = g_value_dup_object (value);
+        break;
+
+      case PROP_INTERPRETER_HOST_PROVIDER:
+        /* Construct-only */
+        g_return_if_fail (self->interpreter_host_provider == NULL);
+        self->interpreter_host_provider = g_value_dup_object (value);
         break;
 
       case PROP_ORIGINAL_ENVIRON:
@@ -1548,14 +1561,16 @@ enumeration_thread_start_arch (EnumerationThread *self,
 static void
 enumeration_thread_start_indep (EnumerationThread *self,
                                 PvRuntimeFlags flags,
-                                PvGraphicsProvider *provider)
+                                PvGraphicsProvider *provider,
+                                const gchar *thread_name)
 {
   g_return_if_fail (self->cancellable == NULL);
   g_return_if_fail (self->system_info == NULL);
   g_return_if_fail (self->thread == NULL);
 
   self->cancellable = g_cancellable_new ();
-  self->thread = g_thread_new ("cross-architecture", enumerate_indep,
+  self->thread = g_thread_new (thread_name == NULL ? "cross-architecture" : thread_name,
+                               enumerate_indep,
                                enumeration_thread_inputs_new (NULL, flags,
                                                               provider,
                                                               self->cancellable));
@@ -1593,7 +1608,15 @@ pv_runtime_initable_init (GInitable *initable,
 
       enumeration_thread_start_indep (&self->indep_thread,
                                       self->flags,
-                                      self->provider);
+                                      self->provider, NULL);
+
+      if (self->interpreter_host_provider != NULL)
+        {
+          enumeration_thread_start_indep (&self->host_thread,
+                                          self->flags,
+                                          self->interpreter_host_provider,
+                                          "real-host");
+        }
 
       self->arch_threads = g_new0 (EnumerationThread,
                                    PV_N_SUPPORTED_ARCHITECTURES);
@@ -1912,7 +1935,9 @@ pv_runtime_dispose (GObject *object)
   PvRuntime *self = PV_RUNTIME (object);
 
   g_clear_object (&self->provider);
+  g_clear_object (&self->interpreter_host_provider);
   enumeration_thread_clear (&self->indep_thread);
+  enumeration_thread_clear (&self->host_thread);
   enumeration_threads_clear (&self->arch_threads,
                              PV_N_SUPPORTED_ARCHITECTURES);
 
@@ -1976,6 +2001,14 @@ pv_runtime_class_init (PvRuntimeClass *cls)
                          (G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY |
                           G_PARAM_STATIC_STRINGS));
 
+  properties[PROP_INTERPRETER_HOST_PROVIDER] =
+    g_param_spec_object ("interpreter-host-provider",
+                         "Interpreter host graphics provider",
+                         "Sysroot used for the interpreter host graphics stack, or NULL",
+                         PV_TYPE_GRAPHICS_PROVIDER,
+                         (G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY |
+                          G_PARAM_STATIC_STRINGS));
+
   properties[PROP_ORIGINAL_ENVIRON] =
     g_param_spec_boxed ("original-environ", "Original environ",
                         "The original environ to use",
@@ -2021,6 +2054,7 @@ pv_runtime_new (const char *source,
                 const char *variable_dir,
                 const char *bubblewrap,
                 PvGraphicsProvider *provider,
+                PvGraphicsProvider *interpreter_host_provider,
                 const GStrv original_environ,
                 PvRuntimeFlags flags,
                 GError **error)
@@ -2033,6 +2067,7 @@ pv_runtime_new (const char *source,
                          error,
                          "bubblewrap", bubblewrap,
                          "graphics-provider", provider,
+                         "interpreter-host-provider", interpreter_host_provider,
                          "original-environ", original_environ,
                          "variable-dir", variable_dir,
                          "source", source,
@@ -6493,7 +6528,9 @@ pv_runtime_use_provider_graphics_stack (PvRuntime *self,
   g_autoptr(GString) va_api_path = g_string_new ("");
   gboolean any_architecture_works = FALSE;
   g_autoptr(SrtSystemInfo) system_info = NULL;
+  g_autoptr(SrtSystemInfo) host_system_info = NULL;
   g_autoptr(IcdStack) provider_stack = icd_stack_new ();
+  g_autoptr(IcdStack) host_stack = icd_stack_new ();
   G_GNUC_UNUSED g_autoptr(SrtProfilingTimer) timer = NULL;
   g_autoptr(SrtProfilingTimer) part_timer = NULL;
   gboolean all_libglx_from_provider = TRUE;
@@ -6525,9 +6562,17 @@ pv_runtime_use_provider_graphics_stack (PvRuntime *self,
     return FALSE;
 
   if (self->flags & PV_RUNTIME_FLAGS_SINGLE_THREAD)
-    system_info = pv_graphics_provider_create_system_info (self->provider);
+    {
+      system_info = pv_graphics_provider_create_system_info (self->provider);
+      if (self->interpreter_host_provider != NULL)
+        host_system_info = pv_graphics_provider_create_system_info (self->interpreter_host_provider);
+    }
   else
-    system_info = g_object_ref (enumeration_thread_join (&self->indep_thread));
+    {
+      system_info = g_object_ref (enumeration_thread_join (&self->indep_thread));
+      if (self->interpreter_host_provider != NULL)
+        host_system_info = g_object_ref (enumeration_thread_join (&self->host_thread));
+    }
 
   provider_stack->egl_icd_details = pv_enumerate_egl_icds (system_info,
                                                            pv_multiarch_tuples, provider);
@@ -6543,6 +6588,22 @@ pv_runtime_use_provider_graphics_stack (PvRuntime *self,
                                          provider,
                                          &provider_stack->vulkan_exp_layer_details,
                                          &provider_stack->vulkan_imp_layer_details);
+    }
+
+  if (host_system_info != NULL)
+    {
+      const gchar *which = "host";
+      host_stack->egl_icd_details = pv_enumerate_egl_icds (host_system_info, NULL, which);
+      host_stack->egl_ext_platform_details = pv_enumerate_egl_ext_platforms (host_system_info,
+                                                                             NULL, which);
+      host_stack->vulkan_icd_details = pv_enumerate_vulkan_icds (host_system_info, NULL, which);
+      if (self->flags & PV_RUNTIME_FLAGS_IMPORT_VULKAN_LAYERS)
+        {
+          pv_enumerate_vulkan_layer_details (host_system_info,
+                                             which,
+                                             &host_stack->vulkan_exp_layer_details,
+                                             &host_stack->vulkan_imp_layer_details);
+        }
     }
 
   /* We set this FALSE later if we decide not to use the provider libc
@@ -6843,15 +6904,46 @@ pv_runtime_use_provider_graphics_stack (PvRuntime *self,
                                  provider_stack->egl_icd_details, egl_path, error))
     return FALSE;
 
+  if (host_stack->egl_icd_details != NULL)
+    {
+      for (i = 0; i < host_stack->egl_icd_details->len; i++)
+        {
+          IcdDetails *details = g_ptr_array_index (host_stack->egl_icd_details, i);
+          SrtEglIcd *icd = SRT_EGL_ICD (details->icd);
+          pv_search_path_append (egl_path, srt_egl_icd_get_json_path (icd));
+        }
+    }
+
   if (!setup_each_json_manifest (self, bwrap, "egl/egl_external_platform.d",
                                  provider_stack->egl_ext_platform_details,
                                  egl_ext_platform_path, error))
     return FALSE;
 
+  if (host_stack->egl_ext_platform_details != NULL)
+    {
+      for (i = 0; i < host_stack->egl_ext_platform_details->len; i++)
+        {
+          IcdDetails *details = g_ptr_array_index (host_stack->egl_ext_platform_details, i);
+          SrtEglExternalPlatform *ext_platform = SRT_EGL_EXTERNAL_PLATFORM (details->icd);
+          pv_search_path_append (egl_ext_platform_path,
+                                 srt_egl_external_platform_get_json_path (ext_platform));
+        }
+    }
+
   g_debug ("Setting up Vulkan ICD JSON...");
   if (!setup_each_json_manifest (self, bwrap, "vulkan/icd.d",
                                  provider_stack->vulkan_icd_details, vulkan_path, error))
     return FALSE;
+
+  if (host_stack->vulkan_icd_details != NULL)
+    {
+      for (i = 0; i < host_stack->vulkan_icd_details->len; i++)
+        {
+          IcdDetails *details = g_ptr_array_index (host_stack->vulkan_icd_details, i);
+          SrtVulkanIcd *icd = SRT_VULKAN_ICD (details->icd);
+          pv_search_path_append (vulkan_path, srt_vulkan_icd_get_json_path (icd));
+        }
+    }
 
   if (self->flags & PV_RUNTIME_FLAGS_IMPORT_VULKAN_LAYERS)
     {
@@ -6861,11 +6953,31 @@ pv_runtime_use_provider_graphics_stack (PvRuntime *self,
                                      vulkan_exp_layer_path, error))
         return FALSE;
 
+      if (host_stack->vulkan_exp_layer_details != NULL)
+        {
+          for (i = 0; i < host_stack->vulkan_exp_layer_details->len; i++)
+            {
+              IcdDetails *details = g_ptr_array_index (host_stack->vulkan_exp_layer_details, i);
+              SrtVulkanLayer *layer = SRT_VULKAN_LAYER (details->icd);
+              pv_search_path_append (vulkan_path, srt_vulkan_layer_get_json_path (layer));
+            }
+        }
+
       g_debug ("Setting up Vulkan implicit layer JSON...");
       if (!setup_each_json_manifest (self, bwrap, "vulkan/implicit_layer.d",
                                      provider_stack->vulkan_imp_layer_details,
                                      vulkan_imp_layer_path, error))
         return FALSE;
+
+      if (host_stack->vulkan_imp_layer_details != NULL)
+        {
+          for (i = 0; i < host_stack->vulkan_imp_layer_details->len; i++)
+            {
+              IcdDetails *details = g_ptr_array_index (host_stack->vulkan_imp_layer_details, i);
+              SrtVulkanLayer *layer = SRT_VULKAN_LAYER (details->icd);
+              pv_search_path_append (vulkan_path, srt_vulkan_layer_get_json_path (layer));
+            }
+        }
     }
 
   if (dri_path->len != 0)

pressure-vessel/runtime.h
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2020 Collabora Ltd.
+ * Copyright © 2020-2022 Collabora Ltd.
  *
  * SPDX-License-Identifier: LGPL-2.1-or-later
  *
@@ -92,6 +92,7 @@ PvRuntime *pv_runtime_new (const char *source,
                            const char *variable_dir,
                            const char *bubblewrap,
                            PvGraphicsProvider *provider,
+                           PvGraphicsProvider *interpreter_host_provider,
                            const GStrv original_environ,
                            PvRuntimeFlags flags,
                            GError **error);

pressure-vessel/supported-architectures.c
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2020 Collabora Ltd.
+ * Copyright © 2020-2022 Collabora Ltd.
  *
  * SPDX-License-Identifier: LGPL-2.1-or-later
  *
@@ -23,6 +23,7 @@
 
 #include "steam-runtime-tools/glib-backports-internal.h"
 #include "steam-runtime-tools/utils-internal.h"
+#include "steam-runtime-tools/architecture.h"
 #include "libglnx.h"
 
 /*
@@ -55,6 +56,7 @@ const PvMultiarchDetails pv_multiarch_details[PV_N_SUPPORTED_ARCHITECTURES] =
 #if defined(__i386__) || defined(__x86_64__)
   {
     .tuple = "x86_64-linux-gnu",
+    .machine_type = SRT_MACHINE_TYPE_X86_64,
     .multilib = { "x86_64-pc-linux-gnu/lib", "lib64", NULL },
     .other_ld_so_cache = { "ld-x86_64-pc-linux-gnu.cache", NULL },
     .platforms = { "xeon_phi", "haswell", "x86_64", NULL },
@@ -62,6 +64,7 @@ const PvMultiarchDetails pv_multiarch_details[PV_N_SUPPORTED_ARCHITECTURES] =
   },
   {
     .tuple = "i386-linux-gnu",
+    .machine_type = SRT_MACHINE_TYPE_386,
     .multilib = { "i686-pc-linux-gnu/lib", "lib32", NULL },
     .other_ld_so_cache = { "ld-i686-pc-linux-gnu.cache", NULL },
     .platforms = { "i686", "i586", "i486", "i386", NULL },
@@ -70,6 +73,7 @@ const PvMultiarchDetails pv_multiarch_details[PV_N_SUPPORTED_ARCHITECTURES] =
 #elif defined(__aarch64__)
   {
     .tuple = "aarch64-linux-gnu",
+    .machine_type = SRT_MACHINE_TYPE_AARCH64,
     .multilib = { "aarch64-unknown-linux-gnueabi/lib", "lib64", NULL },
     .other_ld_so_cache = { "ld-aarch64-unknown-linux-gnueabi.cache", NULL },
     .platforms = { "aarch64", NULL },
@@ -77,11 +81,41 @@ const PvMultiarchDetails pv_multiarch_details[PV_N_SUPPORTED_ARCHITECTURES] =
 #elif defined(_SRT_MULTIARCH)
   {
     .tuple = _SRT_MULTIARCH,
+    .machine_type = SRT_MACHINE_TYPE_UNKNOWN,
   },
 #else
 #error Architecture not supported by pressure-vessel
 #endif
 };
+
+const char * const pv_multiarch_as_emulator_tuples[PV_N_SUPPORTED_ARCHITECTURES_AS_EMULATOR_HOST + 1] =
+{
+  /* The conditional branches here need to be kept in sync with
+   * pv_multiarch_as_emulator_details and
+   * PV_N_SUPPORTED_ARCHITECTURES_AS_EMULATOR_HOST */
+#if defined(__i386__) || defined(__x86_64__)
+  "aarch64-linux-gnu",
+#else
+  /* No supported architectures as emulator */
+#endif
+  NULL
+};
+
+const PvMultiarchDetails pv_multiarch_as_emulator_details[PV_N_SUPPORTED_ARCHITECTURES_AS_EMULATOR_HOST] =
+{
+#if defined(__i386__) || defined(__x86_64__)
+  {
+    .tuple = "aarch64-linux-gnu",
+    .machine_type = SRT_MACHINE_TYPE_AARCH64,
+    .multilib = { "aarch64-unknown-linux-gnueabi/lib", "lib64", NULL },
+    .other_ld_so_cache = { "ld-aarch64-unknown-linux-gnueabi.cache", NULL },
+    .platforms = { "aarch64", NULL },
+  },
+#else
+  /* No supported architectures as emulator */
+#endif
+};
+
 
 /* Architecture-independent ld.so.cache filenames, other than the
  * conventional filename /etc/ld.so.cache used upstream and in Debian
@@ -155,4 +189,24 @@ pv_multiarch_details_get_libdirs (const PvMultiarchDetails *self,
   g_ptr_array_add (dirs, g_strdup ("/usr/lib"));
 
   return g_steal_pointer (&dirs);
+}
+
+/*
+ * Returns: %TRUE if @machine is included in the supported architectures list
+ */
+gboolean
+pv_supported_architectures_include_machine_type (SrtMachineType machine)
+{
+  gsize i;
+
+  if (machine == SRT_MACHINE_TYPE_UNKNOWN)
+    return FALSE;
+
+  for (i = 0; i < G_N_ELEMENTS (pv_multiarch_details); i++)
+    {
+      if (machine == pv_multiarch_details[i].machine_type)
+        return TRUE;
+    }
+
+  return FALSE;
 }

pressure-vessel/supported-architectures.h
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2020 Collabora Ltd.
+ * Copyright © 2020-2022 Collabora Ltd.
  *
  * SPDX-License-Identifier: LGPL-2.1-or-later
  *
@@ -21,6 +21,8 @@
 
 #include <glib.h>
 
+#include "steam-runtime-tools/architecture.h"
+
 #if defined(__i386__) || defined(__x86_64__)
 #define PV_N_SUPPORTED_ARCHITECTURES 2
 #else
@@ -29,11 +31,19 @@
 #define PV_N_SUPPORTED_ARCHITECTURES 1
 #endif
 
+#if defined(__i386__) || defined(__x86_64__)
+#define PV_N_SUPPORTED_ARCHITECTURES_AS_EMULATOR_HOST 1 /* AArch64 */
+#else
+#define PV_N_SUPPORTED_ARCHITECTURES_AS_EMULATOR_HOST 0
+#endif
+
 extern const char * const pv_multiarch_tuples[PV_N_SUPPORTED_ARCHITECTURES + 1];
+extern const char * const pv_multiarch_as_emulator_tuples[PV_N_SUPPORTED_ARCHITECTURES_AS_EMULATOR_HOST + 1];
 
 typedef struct
 {
   const char *tuple;
+  SrtMachineType machine_type;
 
   /* Directories other than /usr/lib that we must search for loadable
    * modules, least-ambiguous first, most-ambiguous last, not including
@@ -60,6 +70,7 @@ typedef struct
 } PvMultiarchDetails;
 
 extern const PvMultiarchDetails pv_multiarch_details[PV_N_SUPPORTED_ARCHITECTURES];
+extern const PvMultiarchDetails pv_multiarch_as_emulator_details[PV_N_SUPPORTED_ARCHITECTURES_AS_EMULATOR_HOST];
 
 extern const char * const pv_other_ld_so_cache[];
 extern const char * const pv_other_ld_so_conf[];
@@ -78,3 +89,5 @@ typedef enum
 
 GPtrArray *pv_multiarch_details_get_libdirs (const PvMultiarchDetails *self,
                                              PvMultiarchLibdirsFlags flags);
+
+gboolean pv_supported_architectures_include_machine_type (SrtMachineType machine);

pressure-vessel/wrap.c
@@ -1003,6 +1003,7 @@ main (int argc,
   int result;
   PvAppendPreloadFlags append_preload_flags = PV_APPEND_PRELOAD_FLAGS_NONE;
   glnx_autofd int root_fd = -1;
+  SrtMachineType host_machine = SRT_MACHINE_TYPE_UNKNOWN;
 
   setlocale (LC_ALL, "");
 
@@ -1097,7 +1098,7 @@ main (int argc,
   if (opt_verbose)
     _srt_util_set_glib_log_handler (G_LOG_DOMAIN, opt_verbose);
 
-  pv_wrap_detect_virtualization (&interpreter_root, NULL);
+  pv_wrap_detect_virtualization (&interpreter_root, &host_machine);
 
   /* Specifying either one of these mutually-exclusive options as a
    * command-line option disables use of the environment variable for
@@ -1564,6 +1565,7 @@ main (int argc,
       G_GNUC_UNUSED g_autoptr(SrtProfilingTimer) timer =
         _srt_profiling_start ("Setting up runtime");
       g_autoptr(PvGraphicsProvider) graphics_provider = NULL;
+      g_autoptr(PvGraphicsProvider) interpreter_host_provider = NULL;
       PvRuntimeFlags flags = PV_RUNTIME_FLAGS_NONE;
       g_autofree gchar *runtime_resolved = NULL;
       const char *runtime_path = NULL;
@@ -1601,7 +1603,23 @@ main (int argc,
         flags |= PV_RUNTIME_FLAGS_FLATPAK_SUBSANDBOX;
 
       if (interpreter_root != NULL)
-        flags |= PV_RUNTIME_FLAGS_INTERPRETER_ROOT;
+        {
+          flags |= PV_RUNTIME_FLAGS_INTERPRETER_ROOT;
+
+          /* Also include the real host graphics stack to allow thunking.
+           * To avoid enumerating the same DRIs/layers twice, we only do
+           * this if the host is not a supported architecture. */
+          if (!pv_supported_architectures_include_machine_type (host_machine))
+            {
+              /* The trailing slash is needed to allow open(2) to work even if
+               * it's using the O_NOFOLLOW flag. */
+              interpreter_host_provider = pv_graphics_provider_new ("/proc/self/root/",
+                                                                    "/proc/self/root/",
+                                                                    FALSE, error);
+              if (interpreter_host_provider == NULL)
+                goto out;
+            }
+        }
 
       if (opt_runtime != NULL)
         {
@@ -1639,6 +1657,7 @@ main (int argc,
                                 opt_variable_dir,
                                 bwrap_executable,
                                 graphics_provider,
+                                interpreter_host_provider,
                                 original_environ,
                                 flags,
                                 error);

tests/pressure-vessel/wrap-setup.c
@@ -172,6 +172,7 @@ fixture_create_runtime (Fixture *f,
                             f->var,
                             NULL,
                             graphics_provider,
+                            NULL,
                             environ,
                             (flags
                              | PV_RUNTIME_FLAGS_VERBOSE

commit 407b3dad8989efc25c4640dc8b94172ba22caf28

pv-runtime: Don't append layers to VK_DRIVER_FILES under FEX-Emu
Layers are not drivers, and their search path is different.

Fixes: 451544d "Add support for FEX-Emu graphics thunking"
Signed-off-by: Simon McVittie <smcv@collabora.com>

pressure-vessel/runtime.c
@@ -7241,7 +7241,7 @@ pv_runtime_use_provider_graphics_stack (PvRuntime *self,
             {
               IcdDetails *details = g_ptr_array_index (host_stack->vulkan_exp_layer_details, i);
               SrtVulkanLayer *layer = SRT_VULKAN_LAYER (details->icd);
-              pv_search_path_append (vulkan_path, srt_vulkan_layer_get_json_path (layer));
+              pv_search_path_append (vulkan_exp_layer_path, srt_vulkan_layer_get_json_path (layer));
             }
         }
 
@@ -7257,7 +7257,7 @@ pv_runtime_use_provider_graphics_stack (PvRuntime *self,
             {
               IcdDetails *details = g_ptr_array_index (host_stack->vulkan_imp_layer_details, i);
               SrtVulkanLayer *layer = SRT_VULKAN_LAYER (details->icd);
-              pv_search_path_append (vulkan_path, srt_vulkan_layer_get_json_path (layer));
+              pv_search_path_append (vulkan_imp_layer_path, srt_vulkan_layer_get_json_path (layer));
             }
         }
     }